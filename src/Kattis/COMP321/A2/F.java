039c51d0d2dba166758e055c37db8c79b6b0a8f4
z1chh <lolchen2012@gmail.com>
2022-09-10 18:52:57 -0400

    F long implementation

--- src/Kattis/COMP321/A2/F.java
+++ src/Kattis/COMP321/A2/F.java
@@ -16,16 +16,32 @@ public class F {
         // Test cases
         int[] seq;
         String[] input;
+        int length;
         for (int i = 0; i < testCases; i++) {
             // Get input
             scanner.nextLine(); // Get rid of carriage return
+            scanner.nextLine(); // Get rid of carriage return
             input = scanner.nextLine().split("\\s");
-            seq = new int[input.length];
-            for (int j = 0; j < input.length; j++) {
+            length = input.length;
+            seq = new int[length];
+            for (int j = 0; j < length; j++) {
                 seq[j] = Integer.parseInt(input[j]);
             }
 
-            // Uh idk
+            // UH IDK
+            // Lazy long inefficient implementation
+            int ct = 0, curSum, count = 0;
+            while (ct < length) {
+                curSum = 0;
+                for (int j = ct; j < length; j++) {
+                    // TO-DO
+                    curSum += seq[j];
+                    if (curSum == 47)
+                        count++;
+                }
+                ct++;
+            }
+            System.out.println(count);
         }
     }
 }
