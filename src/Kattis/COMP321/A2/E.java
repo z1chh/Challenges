9ab011fbc4a69a8826cf9810a3f704766270dad9
z1chh <lolchen2012@gmail.com>
2022-09-10 11:35:27 -0400

    TO-DO: finish problem E

--- src/Kattis/COMP321/A2/E.java
+++ src/Kattis/COMP321/A2/E.java
@@ -1,6 +1,7 @@
 package Kattis.COMP321.A2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class E {
@@ -16,6 +17,8 @@ public class E {
         // Test case
         int numFriendshipsFormed;
         ArrayList<String> friendshipsFormed = new ArrayList<>();
+        HashMap<String, ArrayList<String>> connections = new HashMap<>();
+        String friend1, friend2;
         for (int i = 0; i < testCases; i++) {
             // Get new friendships
             numFriendshipsFormed = scanner.nextInt();
@@ -26,9 +29,25 @@ public class E {
             // Check new friendships
             for (String friendship: friendshipsFormed) {
                 String[] friends = friendship.split("\\s");
+                friend1 = friends[0];
+                friend2 = friends[1];
                 // Check if tuple already existed, and ignore if yes
                 // Check if friends already exist in other groups, if not create new group, otherwise add to other group
                 // Output group size
+                if (connections.containsKey(friend1)) {
+                    if (!connections.get(friend1).contains(friend2))
+                        connections.get(friend1).add(friend2);
+                } else {
+                    connections.put(friend1, new ArrayList<>());
+                    connections.get(friend1).add(friend2);
+                }
+                if (connections.containsKey(friend2)) {
+                    if (!connections.get(friend2).contains(friend1))
+                        connections.get(friend2).add(friend1);
+                } else {
+                    connections.put(friend2, new ArrayList<>());
+                    connections.get(friend2).add(friend1);
+                }
             }
         }
     }
