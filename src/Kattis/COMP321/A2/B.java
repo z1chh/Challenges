4413ebe89f129fa4718e63359caf811a7d4ff926
z1chh <lolchen2012@gmail.com>
2022-09-10 11:03:31 -0400

    Finished Problem B (backspace)

---
+++ src/Kattis/COMP321/A2/B.java
@@ -0,0 +1,38 @@
+package Kattis.COMP321.A2;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+        System.out.println(backspace());
+    }
+
+    private static String backspace() {
+        // Get input
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        StringBuilder output = new StringBuilder();
+
+        // Format input
+        LinkedList<Character> outputList = new LinkedList<>();
+        int size = 0; // To avoid computing outputList.size() many times
+        char cur;
+        for (int i = 0; i < input.length(); i++) {
+            cur = input.charAt(i);
+            if (cur == '<' && size > 0) {
+                outputList.removeLast();
+            } else if (cur != '<') {
+                outputList.addLast(cur);
+                size++;
+            }
+        }
+
+        // Create output
+        for (char c: outputList) {
+            output.append(c);
+        }
+
+        return output.toString();
+    }
+}
