aaccffd9ce37f470a9eec4ee5af252f7a2023862
z1chh <lolchen2012@gmail.com>
2022-09-18 22:47:07 -0400

    I CAN'T FIX A3C: Time limit exceeded

--- src/Kattis/COMP321/A3/C.java
+++ src/Kattis/COMP321/A3/C.java
@@ -13,68 +13,59 @@ public class C {
         Scanner scanner = new Scanner(System.in);
 
         // Variables
-        int numCalls, numIntervals, operators, curOperators;
+        int numCalls, numIntervals, operators, curOperators, start, duration, curTime;
         String[] input;
-        int[][] phoneCalls, intervals;
-        int[] call, interval;
         HashMap<Integer, Integer> dp;
 
         // Get test case
         input = scanner.nextLine().split("\\s");
-        while (!input[0].equals("0") && !input[1].equals("0")) {
-            // Get number of phone calls and intervals
-            numCalls = Integer.parseInt(input[0]);
-            numIntervals = Integer.parseInt(input[1]);
 
+        // Get number of phone calls and intervals
+        numCalls = Integer.parseInt(input[0]);
+        numIntervals = Integer.parseInt(input[1]);
+
+        while (numCalls != 0 && numIntervals != 0) {
             // Get phone calls
-            phoneCalls = new int[numCalls][];
+            dp = new HashMap<>();
             for (int i = 0; i < numCalls; i++) {
                 input = scanner.nextLine().split("\\s");
-                call = new int[2];
-                for (int j = 2; j < 4; j++) {
-                    call[j - 2] = Integer.parseInt(input[j]);
-                }
-                phoneCalls[i] = call;
-            }
+                start = Integer.parseInt(input[2]);
+                duration = Integer.parseInt(input[3]);
 
-            // Get spying intervals
-            intervals = new int[numIntervals][];
-            for (int i = 0; i < numIntervals; i++) {
-                input = scanner.nextLine().split("\\s");
-                interval = new int[2];
-                for (int j = 0; j < 2; j++) {
-                    interval[j] = Integer.parseInt(input[j]);
-                }
-                intervals[i] = interval;
-            }
-
-            // Get operator per time unit
-            dp = new HashMap<>();
-            for (int[] curCall: phoneCalls) {
-                for (int i = curCall[0]; i < curCall[0] + curCall[1]; i++) {
-                    if (dp.containsKey(i))
-                        dp.put(i, dp.get(i) + 1);
+                // Increment number of active phone calls during [start, start + duration[
+                for (int j = start; j < start + duration; j++) {
+                    if (dp.containsKey(j))
+                        dp.put(j, dp.get(j) + 1);
                     else
-                        dp.put(i, 1);
+                        dp.put(j, 1);
                 }
             }
 
             // Get interval values
-            int curTime;
-            for (int[] curInterval: intervals) {
-                curTime = curInterval[0];
+            for (int i = 0; i < numIntervals; i++) {
+                input = scanner.nextLine().split("\\s");
+                start = Integer.parseInt(input[0]);
+                duration = Integer.parseInt(input[1]);
+                curTime = start;
                 operators = dp.getOrDefault(curTime, 0);
 
-                for (int i = curTime; i < curTime + curInterval[1]; i++) {
-                    curOperators = dp.getOrDefault(i, 0);
+                // Look for minute with most phone calls
+                for (int j = start; j < start + duration; j++) {
+                    curOperators = dp.getOrDefault(j, 0);
                     if (curOperators > operators)
                         operators = curOperators;
                 }
+
+                // Output result
                 System.out.println(operators);
             }
 
             // Get next test case
             input = scanner.nextLine().split("\\s");
+
+            // Get number of phone calls and intervals
+            numCalls = Integer.parseInt(input[0]);
+            numIntervals = Integer.parseInt(input[1]);
         }
     }
 }
