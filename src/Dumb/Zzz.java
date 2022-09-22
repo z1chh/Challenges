4ca5364c3f92ef6e069b546c8920e7ec3e27416b
z1chh <lolchen2012@gmail.com>
2022-09-22 00:55:59 -0400

    Nada

--- src/Dumb/Zzz.java
+++ src/Dumb/Zzz.java
@@ -4,8 +4,10 @@ public class Zzz {
     public static void main(String[] args) {
         try {
             System.out.println("START");
-            Thread.sleep(4000);
-            System.out.println("WRONG");
+            Thread.sleep(1000);
+            System.out.println("1 SEC");
+            Thread.sleep(1000);
+            System.out.println("2 SEC");
         } catch (InterruptedException e) {
             System.out.println("Error: InterruptedException");
         }
