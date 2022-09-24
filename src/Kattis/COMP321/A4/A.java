60e5d25e7a7a906b174dc20a9a21111d68293daa
z1chh <lolchen2012@gmail.com>
2022-09-24 00:58:42 -0400

    A4

--- src/Kattis/COMP321/A4/A.java
+++ src/Kattis/COMP321/A4/A.java
@@ -13,11 +13,19 @@ public class A {
 
         // Variables
         String input;
-        String[] numbers;
-        int numStones;
+        String[] numbersArray;
+        int totalStones, numbers;
+        int[] removedStones;
 
         while (!(input = scanner.nextLine()).equals("")) {
-            numbers = input.split("\\s");
+            // Get input
+            numbersArray = input.split("\\s");
+            totalStones = Integer.parseInt(numbersArray[0]);
+            numbers = Integer.parseInt(numbersArray[1]);
+            removedStones = new int[numbers];
+            for (int i = 0; i < numbers; i++) {
+                removedStones[i] = Integer.parseInt(numbersArray[i + 2]);
+            }
         }
     }
 }
