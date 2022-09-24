6442b5934b41d44ea16b82155d344becea508abe
z1chh <lolchen2012@gmail.com>
2022-09-24 00:01:39 -0400

    A4

---
+++ src/Kattis/COMP321/A4/A.java
@@ -0,0 +1,23 @@
+package Kattis.COMP321.A4;
+
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        radioCommercials();
+    }
+
+    private static void radioCommercials() {
+        // Create scanner object
+        Scanner scanner = new Scanner(System.in);
+
+        // Variables
+        String input;
+        String[] numbers;
+        int numStones;
+
+        while (!(input = scanner.nextLine()).equals("")) {
+            numbers = input.split("\\s");
+        }
+    }
+}
