411ab0890314b43cdae6dc06e8df4d29e1819c63
z1chh <lolchen2012@gmail.com>
2022-09-27 19:52:05 -0400

    A4 - A: Fix Time Limit

--- src/Kattis/COMP321/A4/A.py
+++ src/Kattis/COMP321/A4/A.py
@@ -3,26 +3,25 @@ def radio_commercials():
     # Get input
     num_breaks, break_price = list(map(int, input().split()))
     price_per_break = list(map(int, input().split()))
-    
+
     best_profit = -1
-    
+
     for i in range(num_breaks):
         profit = price_per_break[i] - break_price
         price_per_break[i] = profit
         if profit > best_profit:
             best_profit = profit
-    
-    
+
     # Create new DP array
     dp = []
-    
+
     # Initialize first row
     for i in range(num_breaks - 1):
         profit = price_per_break[i] + price_per_break[i + 1]
         dp.append(profit)
         if profit > best_profit:
             best_profit = profit
-            
+
     # Compute for sequences of breaks
     for i in range(3, num_breaks + 1):
         for j in range(0, num_breaks - i + 1):
@@ -30,7 +29,7 @@ def radio_commercials():
             dp[j] = profit
             if profit > best_profit:
                 best_profit = profit
-                
+
     # Output profit
     print(best_profit)
 
