57307efb050911db8e029193874040465b635ecb
z1chh <lolchen2012@gmail.com>
2022-09-28 16:40:07 -0400

    TO-DO: Finish sqrts modulo a prime

--- src/547_maths/sqrt.py
+++ src/547_maths/sqrt.py
@@ -1,2 +1,14 @@
+from GCD import *
+from exponent import *
+
 def sqrt(p, a):
+    r = (p - 1) / 2
+    r2 = 0
+    
+    while r % 2 == 0:
+        r /= 2
+        r2 /= 2
+        ans1 = exponent(a, r, p)
+        ans2 = exponent(b, r2, p)
+        ans = GCD(ans1 * ans2, p)
     return -1
