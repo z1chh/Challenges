84748d1c2460de76c082644a2a791b64768201cf
z1chh <lolchen2012@gmail.com>
2022-09-29 13:07:39 -0400

    Fixed primitive element of F_p

--- src/547_maths/primitive.py
+++ src/547_maths/primitive.py
@@ -1,14 +1,14 @@
-import math
+from exponent import *
 
 def primitive(q, pf):
     for i in range(len(pf)):
         pf[i] = (q - 1) / pf[i]
     for g in range(q):
-        if math.pow(g, q - 1) != 1:
+        if exponent(g, q - 1, q) != 1:
             continue
         valid = True
         for i in range(len(pf)):
-            if math.pow(g, pf[i]) == 1:
+            if exponent(g, pf[i], q) == 1:
                 valid = False
                 break
         if valid:
@@ -16,4 +16,4 @@ def primitive(q, pf):
     return -1
 
 if __name__ == "__main__":
-    print(primitive(5, [2, 2]))
+    print(primitive(7, [2]))
