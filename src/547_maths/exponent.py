947af563fc554f961d7d8a40640013ab85e1f2fb
z1chh <lolchen2012@gmail.com>
2022-09-29 14:22:00 -0400

    Added comments for modular exponentiation

--- src/547_maths/exponent.py
+++ src/547_maths/exponent.py
@@ -1,6 +1,10 @@
+# As per algorithm B.13 (Algorithm ModExp for efficient modular exponentiation)
+# Input : Modulus N; base a in Z_N; integer exponent b > 0
+# Output: [a^b mod N]
 def exponent(a, b, N):
     x = a
     t = 1
+    # Maintain the invariant that the answer is [t * x^b mod N]
     while b > 0:
         if b % 2 == 1:
             t = (t * x) % N
