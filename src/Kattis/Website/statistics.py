310bf6e81bd23cda46429b6a3d7cb22ca2636fbc
z1chh <lolchen2012@gmail.com>
2022-09-30 10:22:47 -0400

    Finished Kattis problem statistics

--- src/Kattis/Website/statistics.py
+++ src/Kattis/Website/statistics.py
@@ -1,14 +1,3 @@
-while True:
-    try:
-        print(input())
-    except:
-        break
-
-
-# https://open.kattis.com/problems/statistics?editsubmit=9505386
-# Finished Kattis problem statistics
-
-
 ct = 1
 while True:
     try:
