8ef9baa4c37f8ad553c46a8ab5a17b4018d12220
z1chh <lolchen2012@gmail.com>
2022-09-30 03:03:14 -0400

    TO-DO: Finish Kattis problem statistics

--- src/Kattis/Website/statistics.py
+++ src/Kattis/Website/statistics.py
@@ -4,5 +4,20 @@ while True:
     except:
         break
 
+
 # https://open.kattis.com/problems/statistics?editsubmit=9505386
 # Finished Kattis problem statistics
+
+
+ct = 1
+while True:
+    try:
+        l = list(map(int, input().split()))
+        l = l[1:]
+        max = max(l)
+        min = min(l)
+        print("Case {c}: {min} {max} {range}".format(
+            c=ct, min=min, max=max, range=max - min))
+        ct += 1
+    except:
+        break
