ba8ccb788d28e86b8baf0a8636774909c4b0ef1f
z1chh <lolchen2012@gmail.com>
2022-09-30 03:02:47 -0400

    TO-DO: Finish Kattis problem statistics

--- src/Dumb/dumb.py
+++ src/Dumb/dumb.py
@@ -8,3 +8,40 @@ if False and 1 / 0:
 print(s[3])
 
 print(int(5 / 2))
+
+print(max([1, 2, 3, 4, 5, 6]))
+
+def test():
+    while True:
+        try:
+            print(int(input()))
+        except:
+            break
+
+test()
+
+ct = 1
+while True:
+    try:
+        l = list(map(int, input().split()))
+        l = l[1:]
+        max = max(l)
+        min = min(l)
+        print("Case {c}: {min} {max} {range}".format(
+            c=ct, min=min, max=max, range=max - min))
+        ct += 1
+    except:
+        break
+
+
+import sys
+
+ct = 1
+for i in sys.stdin:
+        l = list(map(int, i.split()))
+        l = l[1:]
+        max = max(l)
+        min = min(l)
+        print("Case {c}: {min} {max} {range}".format(
+            c=ct, min=min, max=max, range=max - min))
+        ct += 1
---
+++ src/Kattis/Website/statistics.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        print(input())
+    except:
+        break
+
+# https://open.kattis.com/problems/statistics?editsubmit=9505386
+# Finished Kattis problem statistics
