14e21af9583fb1fc4e0a3846ba51f0c0a524bf63
z1chh <lolchen2012@gmail.com>
2022-09-30 00:02:53 -0400

    Improved babybites

--- src/Kattis/Website/babybites.py
+++ src/Kattis/Website/babybites.py
@@ -3,11 +3,8 @@ st = input().split()
 fishy = False
 for i in range(size):
     word = st[i]
-    if word == "mumble":
-        continue
-    else:
+    if word != "mumble":
         if int(word) != i + 1:
             fishy = True
             break
-
 print("something is fishy" if fishy else "makes sense")
