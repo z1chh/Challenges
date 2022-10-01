63ab50b34fe732fe5c7704bb17e648f4cf88c197
z1chh <lolchen2012@gmail.com>
2022-10-01 12:32:14 -0400

    Finished A5 Problem A

---
+++ src/Kattis/COMP321/A5/A.py
@@ -0,0 +1,19 @@
+# Get input
+n = int(input())
+l = list(map(int, input().split()))
+
+# Sort l in reverse order (trees that take the longest to grow)
+l.sort()
+l.reverse()
+
+# Longest day, set to -1
+t = -1
+
+# Search for longest day
+for i, d in enumerate(l):
+    # Longest day is start day + time it takes to fully grow
+    if i + 1 + d > t: # (i + 1) since days start at 1, not 0
+        t = i + 1 + d
+
+# Output the results
+print(t + 1) # t + 1 since the party starts the next day only
