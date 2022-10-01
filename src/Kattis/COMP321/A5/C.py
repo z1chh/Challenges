da4520ba97ddc34bf9f8ce4f1852eb6d6d7310a5
z1chh <lolchen2012@gmail.com>
2022-10-01 13:47:14 -0400

    Changed to dict but still TLE

--- src/Kattis/COMP321/A5/C.py
+++ src/Kattis/COMP321/A5/C.py
@@ -1,24 +1,41 @@
 # Get input
-ns, cap, dif = map(int, input().split())
-socks = list(map(int, input().split()))
+num_socks, cap, dif = map(int, input().split())
+ss = list(map(int, input().split()))
+ss.sort()
 
-# Sort socks in color order
-socks.sort()
+# Sort socks in order by color
+socks = {}
+for s in ss:
+    if s in socks.keys():
+        socks[s] += 1
+    else:
+        socks[s] = 1
 
-b = 0
-while socks:
-    c1 = socks[0]  # First color
-    socks.pop(0)  # Create new batch to wash
-    cc = 1  # Number of socks currently in the batch
-    while cc < cap and socks:
-        if socks[0] - c1 <= dif:
-            socks.pop(0)
-            cc += 1
-        else:
-            break
+num_batches = 0
+#print("socks to wash:", socks)
+while num_socks > 0:
+    # Start a batch
+    num_batches += 1
+    
+    # Reset vars
+    current_socks = 0
+    color_diff_reached = False
+    first_color = -1
 
-    # Increment number of batches
-    b += 1
+    # Get socks for the current batch
+    while current_socks < cap and num_socks > 0 and not color_diff_reached:
+        for color in socks.keys():
+            if socks[color] > 0:
+                if first_color == -1:
+                    first_color = color
+                elif color - first_color > dif:
+                    color_diff_reached = True
+                    break
+                to_add = min(socks[color], cap - current_socks)
+                socks[color] -= to_add
+                current_socks += to_add
+                num_socks -= to_add
+    #print("socks to wash:", socks)
 
 # Output results
-print(b)
+print(num_batches)
