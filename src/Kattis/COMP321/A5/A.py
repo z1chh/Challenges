36c5c316482ccce2dc132db7c75ce36432ef8b37
z1chh <lolchen2012@gmail.com>
2022-10-01 14:46:50 -0400

    Tiny improvements to code

--- src/Kattis/COMP321/A5/A.py
+++ src/Kattis/COMP321/A5/A.py
@@ -1,10 +1,9 @@
 # Get input
-n = int(input())
+input()  # Don't care
 l = list(map(int, input().split()))
 
 # Sort l in reverse order (trees that take the longest to grow)
-l.sort()
-l.reverse()
+l.sort(reverse=True)
 
 # Longest day, set to -1
 t = -1
@@ -12,8 +11,8 @@ t = -1
 # Search for longest day
 for i, d in enumerate(l):
     # Longest day is start day + time it takes to fully grow
-    if i + 1 + d > t: # (i + 1) since days start at 1, not 0
+    if i + 1 + d > t:  # (i + 1) since days start at 1, not 0
         t = i + 1 + d
 
 # Output the results
-print(t + 1) # t + 1 since the party starts the next day only
+print(t + 1)  # t + 1 since the party starts the next day only
