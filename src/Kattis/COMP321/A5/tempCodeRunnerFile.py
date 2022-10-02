aef5aa5084e3a8786dc93ae728939d5b7983a1cd
z1chh <lolchen2012@gmail.com>
2022-10-01 23:02:43 -0400

    Nada

--- src/Kattis/COMP321/A5/tempCodeRunnerFile.py
+++ src/Kattis/COMP321/A5/tempCodeRunnerFile.py
@@ -1,15 +1,1 @@
-qs, bc = map(int, input().split())
-q = {}
-pc = []
-for i in range(qs):
-    c, t = map(int, input().split())
-    q.setdefault(t, []).append(c)
-m = 0
-for t in range(bc - 1, -1, -1):
-    if t in q.keys():
-        for c in q[t]:
-            pc.append(c)
-        if pc:
-            m += max(pc)
-            pc.remove(max(pc))
-print(m)
+reverse=True
--- src/Kattis/COMP321/A5/test.py
+++ src/Kattis/COMP321/A5/test.py
@@ -1,15 +1,10 @@
-l = map(int, input().split())
-
-d = {}
-
-for el in l:
-    if el in d.keys():
-        d[el] += 1
-    else:
-        d[el] = 1
-
-l = []
-for val in d.values():
-    l.append(val)
-
+l = [1, 5, 3, 12, 2, 7]
+l2 = l.copy()
 print(l)
+print(l2)
+l2.sort(reverse=True)
+print(l)
+print(l2)
+print(l2.index(12))
+l2.remove(1)
+print(l2)
