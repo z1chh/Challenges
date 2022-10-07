7c02e183d8bcd1e157ab9854ae3c8754bc31daf3
z1chh <lolchen2012@gmail.com>
2022-10-07 00:22:15 -0400

    Fixed dumb clown error :D

--- src/Kattis/COMP321/Assignments/A5/B.py
+++ src/Kattis/COMP321/Assignments/A5/B.py
@@ -1,13 +1,8 @@
-# Greedy
-nc = int(input())
-for i in range(1, nc + 1):
+for i in range(1, int(input()) + 1):
     size = int(input())
-    v1 = list(map(int, input().split()))
-    v2 = list(map(int, input().split()))
-    v1.sort()
-    v2.sort()
-    v2.reverse()
+    v1 = sorted(list(map(int, input().split())))
+    v2 = sorted(list(map(int, input().split())), reverse=True)
     s = 0
     for a, b in zip(v1, v2):
         s += a * b
-    print(s)
+    print(f"Case #{i}: {s}")
