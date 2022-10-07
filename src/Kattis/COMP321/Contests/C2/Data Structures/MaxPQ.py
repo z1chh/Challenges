9820264bcb859c4e20eb5c5bcd628a4620ed0c79
z1chh <lolchen2012@gmail.com>
2022-10-07 00:31:31 -0400

    Added MaxPQ Documentation

--- src/Kattis/COMP321/Contests/C2/Data Structures/MaxPQ.py
+++ src/Kattis/COMP321/Contests/C2/Data Structures/MaxPQ.py
@@ -1,31 +1,50 @@
+"""
+MAX PRIORITY QUEUE
+
+Provides the following functions:
+- add(value) -> Add value to MaxPQ
+- remove()   -> Max P
+- top()      -> Peek at the Max P
+- len()      -> length
+- str()      -> conversion to string
+
+Notes:
+- Initializes as empty MaxPQ
+- Sorts its elements only when removing, peeking or casting to str
+"""
+
+
 class MaxPQ:
     def __init__(self) -> None:
         self.queue = []
         self.size = 0
         self.ordered = True
-    
+
     def add(self, value):
         self.queue.append(value)
         self.size += 1
         self.ordered = False
-    
+
     def remove(self):
         if not self.ordered:
             self.queue.sort()
             self.ordered = True
         self.size -= 1
         return self.queue.pop()
-    
+
     def top(self):
         if not self.ordered:
             self.queue.sort()
             self.ordered = True
         return self.queue[-1]
-    
+
     def __len__(self):
         return self.size
-    
+
     def __repr__(self) -> str:
+        if not self.ordered:
+            self.queue.sort()
+            self.ordered = True
         q = "[ "
         for el in self.queue:
             q += str(el) + " "
