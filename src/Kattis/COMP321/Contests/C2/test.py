faa45bbc09b4042ddcaae86cdda9c6fb7549b721
z1chh <lolchen2012@gmail.com>
2022-10-08 01:23:24 -0400

    sys.stdout

--- src/Kattis/COMP321/Contests/C2/test.py
+++ src/Kattis/COMP321/Contests/C2/test.py
@@ -1,3 +1,6 @@
+import sys
+
+
 a = "ASD"
 if a is not None:
     print("AA")
@@ -7,3 +10,10 @@ print(a)
 
 # TESTING GITHUB EXTENSION VCS FROM LAPTOP
 # ALLO TEST B4 CONTEST
+sys.stderr.write("Example 3\n")
+sys.stdout.write(sys.stdin.readline(2) + "\n")
+sys.stdout.write(sys.stdin.readline(2) + "\n")
+sys.stdout.write(sys.stdin.readline() + "\n")
+
+
+
