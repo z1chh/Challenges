9eb9be6c573b605ae8d58c948f33d6f12e0b147a
z1chh <lolchen2012@gmail.com>
2022-10-14 00:25:43 -0400

    Code doesn't run idk why

---
+++ src/Kattis/COMP321/Assignments/A1_cpp/D.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+void displayArr(char *arr)
+{
+    cout << "Displaying array..." << endl;
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl
+         << "Array displayed..." << endl;
+}
+
+int main()
+{
+    int tc;
+    cin >> tc;
+
+    char *str;
+
+    for (int i = 0; i < tc; i++)
+    {
+        cout << "Case " << i + 1 << endl;
+        cin >> str;
+        displayArr(str);
+        cout << "DONE" << endl;
+        char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        for (int j = 0; j < sizeof(str) / sizeof(str[0]); j++)
+        {
+            char c = str[j];
+            if (c >= 'A' && c <= 'Z')
+            {
+                c += 32;
+            }
+            if (c >= 'a' && c <= 'z')
+            {
+                alphabet[c - 97] = ' ';
+            }
+        }
+        int missing = 0;
+        char missing_letters[26];
+        for (int j = 0; j < 26; j++)
+        {
+            if (alphabet[j] == j + 97)
+            {
+                missing_letters[missing++] = j + 97;
+            }
+        }
+        if (missing == 0)
+        {
+            cout << "pangram" << endl;
+        }
+        else
+        {
+            cout << "missing";
+            for (int j = 0; j < missing; j++)
+            {
+                cout << missing_letters[j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
