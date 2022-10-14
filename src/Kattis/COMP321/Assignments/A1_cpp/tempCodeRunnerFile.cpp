b6e4944dc1bc5c0a905b26aa42928b5b596fba3c
z1chh <lolchen2012@gmail.com>
2022-10-14 00:50:26 -0400

    C.exe

--- src/Kattis/COMP321/Assignments/A1_cpp/tempCodeRunnerFile.cpp
+++ src/Kattis/COMP321/Assignments/A1_cpp/tempCodeRunnerFile.cpp
@@ -4,3 +4,16 @@ using namespace std;
 
 int main()
 {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    if (a > b)
+    {
+        cout << b << " " << a << endl;
+    }
+    else
+    {
+        cout << a << " " << b << endl;
+    }
+    return 0;
+}
