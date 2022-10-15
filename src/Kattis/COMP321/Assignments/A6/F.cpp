fa9a7c4980747723d93ca1c986eb3f705870f89b
z1chh <lolchen2012@gmail.com>
2022-10-15 17:26:43 -0400

    Done A6 Problem F

--- src/Kattis/COMP321/Assignments/A6/F.cpp
+++ src/Kattis/COMP321/Assignments/A6/F.cpp
@@ -26,11 +26,47 @@ int main()
     }
 
     // DP backwards
-    for (int step = destination; step >= 0; step--)
+    int step, square, forward, backward, value;
+    for (step = destination; step >= 0; step--)
     {
-        for (int square = 1; square < destination + 1; square++)
+        for (square = 1; square < destination + 1; square++)
         {
-            // TODO
+            forward = square + step + 1;
+            backward = square - step;
+
+            // Check if square is destination
+            if (square == destination)
+            {
+                dp[step][square] = prices[destination - 1];
+            }
+            
+            // Check if we can move forward
+            if (forward <= destination && step < destination)
+            {
+                if (dp[step + 1][forward] != -1)
+                {
+                    dp[step][square] = dp[step + 1][forward] + prices[square - 1];
+                }
+            }
+
+            // Check if we can move backward
+            if (backward > 0)
+            {
+                if (dp[step][backward] != -1)
+                {
+                    value = dp[step][backward] + prices[square - 1];
+                    if (dp[step][square] == -1 || value < dp[step][square])
+                    {
+                        dp[step][square] = value;
+                    }
+                }
+            }
         }
     }
+
+    // Output results
+    cout << dp[1][2] << endl;
+
+    // Successful exit
+    return 0;
 }
