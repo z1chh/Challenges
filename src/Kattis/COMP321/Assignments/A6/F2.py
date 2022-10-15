348ef7c26ed151ab9ba31b85ed47fbec86a68bbb
z1chh <lolchen2012@gmail.com>
2022-10-15 09:32:40 -0400

    Implemented A6 Problem F

---
+++ src/Kattis/COMP321/Assignments/A6/F2.py
@@ -0,0 +1,24 @@
+import sys
+
+# Get input
+dest = int(sys.stdin.readline().strip())
+prices = [int(line.strip()) for line in sys.stdin]
+dp = [[-1] * (dest + 1) for _ in range(dest + 1)] # Dynamic programming matrix
+
+# Start from the end
+for step in range(dest, -1, -1):
+    for square in range (1, dest + 1):
+        if square == dest:
+            dp[step][square] = prices[-1]
+            continue
+        square_forward = square + step + 1
+        square_backward = square - step
+        if square_forward <= dest and step < dest:
+            if dp[step + 1][square_forward] != -1:
+                dp[step][square] = dp[step + 1][square_forward] + prices[square - 1]
+        if square_backward > 0:
+            if dp[step][square_backward] != -1:
+                value = dp[step][square_backward] + prices[square - 1]
+                if dp[step][square] == -1 or value < dp[step][square]:
+                    dp[step][square] = value
+print(dp[1][2])
