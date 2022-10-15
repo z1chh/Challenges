c34c8e74a786444d2734b08420af85db82a804e2
z1chh <lolchen2012@gmail.com>
2022-10-15 00:31:02 -0400

    Contest 1 Problem F in C++

---
+++ src/Kattis/COMP321/Contests/C1/F.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int times;
+    cin >> times;
+    for (int i = 0; i < times; i++)
+    {
+        cout << i + 1 << " Abracadabra" << endl;
+    }
+    return 0;
+}
