7b70575bad994a882e9518f288a33ea7ec8b1c9b
z1chh <lolchen2012@gmail.com>
2022-10-15 10:38:50 -0400

    A6 Problem D in cpp (does not count)

--- src/Kattis/COMP321/Assignments/A6/Does Not Count/D.cpp
+++ src/Kattis/COMP321/Assignments/A6/Does Not Count/D.cpp
@@ -5,12 +5,20 @@ using namespace std;
 
 int main()
 {
+    // Declare vars
     int test_cases;
     string num;
+
+    // Get input
     cin >> test_cases;
+
+    // Compute magnitude
     for (int i = 0; i < test_cases; i++)
     {
         cin >> num;
         cout << num.size() << endl;
     }
+
+    // Successful return
+    return 0;
 }
