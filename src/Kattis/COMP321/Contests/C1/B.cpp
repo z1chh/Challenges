9e14f255aa3b21e67253aaa167e9ab38e57a7945
z1chh <lolchen2012@gmail.com>
2022-10-15 00:34:21 -0400

    Contest 1 Problem B in C++

---
+++ src/Kattis/COMP321/Contests/C1/B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, n;
+    cin >> a >> b >> c >> n;
+    if (a == 0 || b == 0 || c == 0 || n < 3 || a + b + c < n)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
