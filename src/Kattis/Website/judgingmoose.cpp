19e0fd289fc8f9cf7466b027aebfa29046bd14ad
z1chh <lolchen2012@gmail.com>
2022-10-16 14:07:23 -0400

    Finished Kattis problem judgingmoose

---
+++ src/Kattis/Website/judgingmoose.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int l, r;
+    cin >> l >> r;
+    if (l == 0 && r == 0)
+    {
+        cout << "Not a moose" << endl;
+    }
+    else if (l == r)
+    {
+        cout << "Even " << l * 2 << endl;
+    }
+    else
+    {
+        if (l > r)
+        {
+            cout << "Odd " << l * 2 << endl;
+        }
+        else
+        {
+            cout << "Odd " << r * 2 << endl;
+        }
+    }
+}
