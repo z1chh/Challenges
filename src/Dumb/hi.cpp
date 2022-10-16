6ecd9e6d1589cb08514eb8c4eb968212a612152e
z1chh <lolchen2012@gmail.com>
2022-10-16 11:19:38 -0400

    maps & pairs

--- src/Dumb/hi.cpp
+++ src/Dumb/hi.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
 
-void increment(int *i)
-{
-    (*i)++;
-}
+void increment(int *i);
 
-int javaIncrement(int a)
-{
-    return ++a;
-}
+int javaIncrement(int a);
+
+void t1();
+void t2();
 
 int main()
+{
+    t2();
+}
+
+void t2()
+{
+    map<int, pair<string, int>> m;
+    m[0] = make_pair("Hello", 5);
+    m[1] = make_pair("World!", 10);
+    // it points to nothing at first, then everytime you it++ it points to next element
+    // {1, 2, 3, 4, 5}
+    // it -> Nothing, 1, 2, 3, 4, 5, ERR)
+    for (auto it = m.begin(); it != m.end(); ++it)
+    {
+        cout << it->first << "\t" << it->second.first << "\t" << it->second.second << endl;
+    }
+}
+
+void t1()
 {
     // C++
     int a = 5;
@@ -23,4 +41,14 @@ int main()
     int b = 10;
     b = javaIncrement(b);
     cout << b << endl;
+}
+
+void increment(int *i)
+{
+    (*i)++;
+}
+
+int javaIncrement(int a)
+{
+    return ++a;
 }
