86244def04c11d3f88dbb76337c75f8d18cf6893
z1chh <lolchen2012@gmail.com>
2022-10-16 10:03:38 -0400

    Testing shii

--- src/Kattis/COMP321/Assignments/A6/test.cpp
+++ src/Kattis/COMP321/Assignments/A6/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -23,11 +24,23 @@ void hello();
 void t1();
 void test();
 void t2();
+void t3();
 
 int main()
 {
-    t2();
+    t3();
     return 0;
+}
+
+void t3()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    sort(arr, arr + 5);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
 void t2h(int *arr)
