44dbc04e50b90cbd40fdbcff8db16ca3dbffe14b
z1chh <lolchen2012@gmail.com>
2022-10-16 13:53:37 -0400

    Finished Kattis problem qaly

---
+++ src/Kattis/Website/qaly.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int periods;
+    cin >> periods;
+    double qaly = 0.0;
+    double q, y;
+    for (int i = 0; i < periods; i++)
+    {
+        cin >> q >> y;
+        qaly += q * y;
+    }
+    cout << qaly << endl;
+}
