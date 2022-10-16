88088d9de83deb9f694c00684cfd6a8f0f788b86
z1chh <lolchen2012@gmail.com>
2022-10-16 15:07:31 -0400

    Finished Kattis problem helpaphd

---
+++ src/Kattis/Website/helpaphd.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int a, idx = -1, n1, n2;
+    string s;
+    bool b;
+    cin >> a;
+    for (int i = 0; i < a; i++)
+    {
+        b = false;
+        cin >> s;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '+')
+            {
+                b = true;
+                idx = i;
+                break;
+            }
+            else if (s[i] == '=')
+            {
+                break;
+            }
+        }
+        if (b)
+        {
+            n1 = stoi(s.substr(0, idx));
+            n2 = stoi(s.substr(idx + 1, s.size()));
+            cout << n1 + n2 << endl;
+        }
+        else
+        {
+            cout << "skipped" << endl;
+        }
+    }
+}
