ce2eae77f2daacd639b98797ed89af214c1fdb03
z1chh <lolchen2012@gmail.com>
2022-10-16 14:17:24 -0400

    Finished Kattis problem onechicken

---
+++ src/Kattis/Website/onechicken.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, d;
+    cin >> a >> b;
+    if (a < b)
+    {
+        d = b - a;
+        if (d > 1)
+        {
+            cout << "Dr. Chaz will have " << d << " pieces of chicken left over!" << endl;
+        }
+        else
+        {
+            cout << "Dr. Chaz will have 1 piece of chicken left over!" << endl;
+        }
+    }
+    else
+    {
+        d = a - b;
+        if (d > 1)
+        {
+            cout << "Dr. Chaz needs " << d << " more pieces of chicken!" << endl;
+        }
+        else
+        {
+            cout << "Dr. Chaz needs 1 more piece of chicken!" << endl;
+        }
+    }
+}
