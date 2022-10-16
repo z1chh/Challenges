4b4466c2e57b50faef5ec304ffeb2577722006ed
z1chh <lolchen2012@gmail.com>
2022-10-16 00:11:38 -0400

    Testing stuff

--- src/Kattis/COMP321/Assignments/A6/test.cpp
+++ src/Kattis/COMP321/Assignments/A6/test.cpp
@@ -8,41 +8,52 @@ class Student
 {
 public:
     string name;
-    int id;
 
     Student(string name) : name(name)
     {
-        id = student_id++;
     }
-
-    /* friend ostream &operator<<(ostream &strm, const Student &s)
-    {
-        return s.name; //+ " (" + s.id + ")";
-    } */
 
     void print_name()
     {
         cout << "Student name is " << name << endl;
     }
-
-    void print_id()
-    {
-        cout << "Student ID is " << id << endl;
-    }
 };
+// Functions
+void hello();
+void t1helper();
+void t1();
+void test();
+
+int main()
+{
+    t1();
+    return 0;
+}
+
+void t1helper(int *a)
+{
+    ++a;
+}
+
+void t1()
+{
+    int a = 5;
+    cout << a << endl;
+    t1helper(&a);
+    cout << a << endl;
+    t1helper(&a);
+    cout << a << endl;
+}
+
+void hello()
+{
+    cout << "Hello World!" << endl;
+}
 
 void test()
 {
     Student s = Student("Zichh");
     s.print_name();
-    s.print_id();
     Student s2("Stub");
     s2.print_name();
-    s2.print_id();
-}
-
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
 }
