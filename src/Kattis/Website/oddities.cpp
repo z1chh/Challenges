3c8546cf768461860eb5c66d8d6ca03bb8a3624c
z1chh <lolchen2012@gmail.com>
2022-10-16 14:48:29 -0400

    Finished Kattis problem oddities

---
+++ src/Kattis/Website/oddities.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a;
+    for (int i = 0; i < a; i++)
+    {
+        cin >> b;
+        if (b & 1) {
+            cout << b << " is odd" << endl;
+        } else {
+            cout << b << " is even" << endl;
+        }
+    }
+}
