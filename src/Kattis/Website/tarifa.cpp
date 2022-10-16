7c1400455429c33ada91e16605453a4ecc6fe401
z1chh <lolchen2012@gmail.com>
2022-10-16 13:59:15 -0400

    Finished Kattis problem tarifa

---
+++ src/Kattis/Website/tarifa.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, n, p, tr;
+    cin >> x >> n;
+    tr = x * (n + 1);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> p;
+        tr -= p;
+    }
+    cout << tr << endl;
+}
