47bd79fee4f615ba1e4fac0b71a5436f1d73ebe0
z1chh <lolchen2012@gmail.com>
2022-10-17 00:28:10 -0400

    done?

--- src/What is French/nombres_francais.py
+++ src/What is French/nombres_francais.py
@@ -62,3 +62,50 @@ def nombreFrancais19(n):
         return nombreDizaineFrancais6(1) + "-" + nombreFrancais9(n - 10)
     else:
         raise ValueError("input must be between 1 and 19")
+
+
+def nombreFrancais99(n):
+    if n < 20:
+        return nombreFrancais19(n)
+    elif n >= 20 and n <= 59:
+        if n % 10 == 1:
+            return nombreDizaineFrancais6(n // 10) + "-et-" + nombreFrancais9(n % 10)
+        elif n % 10 == 0:
+            return nombreDizaineFrancais6(n // 10)
+        else:
+            return nombreDizaineFrancais6(n // 10) + "-" + nombreFrancais9(n % 10)
+    elif n >= 60 and n <= 69:
+        if n % 10 == 1:
+            return nombreDizaineFrancais6(6) + "-et-" + nombreFrancais19(n % 10)
+        elif n % 10 == 0:
+            return nombreDizaineFrancais6(6)
+        else:
+            return nombreDizaineFrancais6(6) + "-" + nombreFrancais9(n % 10)
+    elif n == 70:
+        return nombreDizaineFrancais6(6) + "-" + nombreDizaineFrancais6(1)
+    elif n > 70 and n <= 79:
+        if n % 10 == 1:
+            return nombreDizaineFrancais6(6) + "-et-" + nombreFrancais19(11)
+        else:
+            return nombreDizaineFrancais6(6) + "-" + nombreFrancais19(n - 60)
+    elif n == 80:
+        return nombreFrancais9(4) + "-" + nombreDizaineFrancais6(2)
+    elif n > 80 and n <= 89:
+        if n % 10 == 1:
+            return nombreFrancais9(4) + "-" + nombreDizaineFrancais6(2) + "-et-" + nombreFrancais9(n - 80)
+        else:
+            return nombreFrancais9(4) + "-" + nombreDizaineFrancais6(2) + "-" + nombreFrancais19(n - 80)
+    elif n >= 90 and n <= 99:
+        if n % 10 == 1:
+            return nombreFrancais9(4) + "-" + nombreDizaineFrancais6(2) + "-et-" + nombreFrancais19(11)
+        else:
+            return nombreFrancais9(4) + "-" + nombreDizaineFrancais6(2) + "-" + nombreFrancais19(n - 80)
+    else:
+        raise ValueError("input must be between 1 and 99")
+
+
+if __name__ == "__main__":
+    l = [1, 8, 12, 16, 19, 20, 30, 41, 56, 60, 61, 69,
+         70, 71, 75, 79, 80, 81, 88, 90, 91, 95, 99]
+    for i in l:
+        print(nombreFrancais99(i))
---
+++ src/What is French/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if n % 10 == 1:
+            return nombreFrancais9(4) + "-" + nombreDizaineFrancais6(2) + "-et-" + nombreFrancais19(n - 80)
+        else:
+            
