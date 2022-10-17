4d4eda651f5ab5f7f95da49882dcb85e31d1f427
z1chh <lolchen2012@gmail.com>
2022-10-17 00:05:52 -0400

    bro who does that

--- src/What is French/nombres_francais.py
+++ src/What is French/nombres_francais.py
@@ -35,3 +35,27 @@ def nombreDizaineFrancais6(n):
         return "soixante"
     else:
         raise ValueError("input must be between 1 and 6")
+
+def nombreFrancais19(n):
+    if n <= 0:
+        raise ValueError("input must be between 1 and 19")
+    if n < 10:
+        return nombreFrancais9(n)
+    if n == 10:
+        return nombreDizaineFrancais6(1)
+    if n == 11:
+        return "onze"
+    if n == 12:
+        return "douze"
+    if n == 13:
+        return "treize"
+    if n == 14:
+        return "quatorze"
+    if n == 15:
+        return "quinze"
+    if n == 16:
+        return "seize"
+    if n >= 17 and n <= 19:
+        return nombreDizaineFrancais6(1) + "-" + nombreFrancais9(n - 10)
+    else:
+        raise ValueError("input must be between 1 and 19")
