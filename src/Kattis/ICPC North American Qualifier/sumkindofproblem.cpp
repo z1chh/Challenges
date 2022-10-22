6f689a521f72813b00b3da563373cfc5d3bd896a
z1chh <lolchen2012@gmail.com>
2022-10-22 00:26:17 -0400

    Improved algorithm for better runtime

--- src/Kattis/ICPC North American Qualifier/sumkindofproblem.cpp
+++ src/Kattis/ICPC North American Qualifier/sumkindofproblem.cpp
@@ -5,21 +5,14 @@ using namespace std;
 int main()
 {
     int tc;
-    scanf("%d", &tc);
+    cin >> tc;
     for (int i = 0; i < tc; i++)
     {
         int testnum;
-        scanf("%d", &testnum);
+        cin >> testnum;
         int N;
-        scanf("%d", &N);
-        int s1 = 0, s2 = 0, s3 = 0;
-        for (int i = 1; i <= N; i++)
-        {
-            s1 += i;
-            s2 += (i - 1) * 2 + 1;
-            s3 += i * 2;
-        }
-        printf("%d %d %d %d\n", testnum, s1, s2, s3);
+        cin >> N;
+        cout << testnum << " " << N * (N + 1) / 2 << " " << N * N << " " << N * (N + 1) << endl;
     }
     return 0;
 }
