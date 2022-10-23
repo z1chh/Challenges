f8dbbb87f21ba8bcb7b9571ddb65ad6745dec119
z1chh <lolchen2012@gmail.com>
2022-10-23 00:06:43 -0400

    Started A7 Problem A

---
+++ src/Kattis/Programming Challenges/Assignments/A7/A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int height, max_idx = 0, row_nodes, idx = 0;
+    pair<int, int> nodes_per_level[height + 1];
+    string path;
+    cin >> height >> path;
+
+    for (int i = height; i >= 0; i--)
+    {
+        row_nodes = pow(2, i);
+        max_idx += row_nodes;
+        nodes_per_level[height] = pair(row_nodes, max_idx);
+    }
+}
+
+// int max_node=2ala height + 1
