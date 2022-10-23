247891abbb62da0d12e33b1ecc3807355d283565
z1chh <lolchen2012@gmail.com>
2022-10-22 20:17:58 -0400

    NP-Compete Contest Problem A

--- src/Kattis/NP-Compete McGill/A.py
+++ src/Kattis/NP-Compete McGill/A.py
@@ -1,5 +1,11 @@
-# Useful code
-
-# int(input())
-# map(int, input().split())
-# list(map(int, input().split()))
+n = int(input())
+toreturn = []
+result = []
+for i in range(1, int(n**0.5)+1):
+    if n % i == 0:
+        result.append(i - 1)
+        result.append(n//i - 1)
+for num in sorted(result):
+    if str(num) not in toreturn:
+        toreturn.append(str(num))
+print(' '.join(toreturn))
