251b1b8f33ffea7edd3fb11a17d89e755771ee4b
z1chh <lolchen2012@gmail.com>
2022-10-27 09:14:05 -0400

    Confusion

--- src/Kattis/Programming Challenges/Assignments/A7/A.cpp
+++ src/Kattis/Programming Challenges/Assignments/A7/A.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <utility>
 
 using namespace std;
 
 int main()
 {
     int height, max_idx = 0, row_nodes, idx = 0;
-    pair<int, int> nodes_per_level[height + 1];
+    pair<int, pair<int, int>> nodes_per_level[height + 1];
     string path;
     cin >> height >> path;
 
@@ -15,7 +16,7 @@ int main()
     {
         row_nodes = pow(2, i);
         max_idx += row_nodes;
-        nodes_per_level[height] = pair(row_nodes, max_idx);
+        nodes_per_level[i] = make_pair(row_nodes, max_idx);
     }
 }
 
