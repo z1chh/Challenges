c07dfe1cc9bbaafbfd2193044bd9dcffdd80ea55
z1chh <lolchen2012@gmail.com>
2022-10-27 10:11:34 -0400

    i think its working? except with edge case

--- src/Kattis/Programming Challenges/Assignments/A7/A.cpp
+++ src/Kattis/Programming Challenges/Assignments/A7/A.cpp
@@ -10,21 +10,35 @@ int main()
     int height, max_idx = 0, row_nodes, idx = 0, tmp;
     string path;
     cin >> height >> path;
-    pair<int, pair<int, int>> nodes_per_level[height + 1];
+    pair<int, int> nodes_per_level[height + 1];
     row_nodes = pow(2, height);
 
     for (int i = height; i >= 0; i--)
     {
-        tmp = max_idx + 1;
+        nodes_per_level[i] = make_pair(row_nodes, max_idx + 1);
         max_idx += row_nodes;
-        nodes_per_level[i] = make_pair(row_nodes, make_pair(tmp, max_idx));
         row_nodes = row_nodes >> 1; // not SRL but okay since we're working with positive ints (also powers of 2)
     }
 
     for (int i = 0; i <= height; i++)
     {
-        cout << i << ": " << nodes_per_level[i].first << " " << nodes_per_level[i].second.first << " " << nodes_per_level[i].second.second << endl;
+        cout << i << ": " << nodes_per_level[i].first << " " << nodes_per_level[i].second << endl;
     }
+
+    // Get correct node
+    int size = path.size();
+    pair<int, int> level = nodes_per_level[size];
+    int num_nodes = level.first;
+    int index = 0;
+    for (char c: path)
+    {
+        num_nodes = num_nodes >> 1;
+        if (c == 'L')
+        {
+            index += num_nodes;
+        }
+    }
+    cout << level.second + index << endl;
 
     return 0;
 }
