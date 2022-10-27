be47ff9e36ef2ab07490ad4c3df8dc2f46dfbfa4
z1chh <lolchen2012@gmail.com>
2022-10-27 09:23:38 -0400

    code stops executing

--- src/Kattis/Programming Challenges/Assignments/A7/A.cpp
+++ src/Kattis/Programming Challenges/Assignments/A7/A.cpp
@@ -7,16 +7,23 @@ using namespace std;
 
 int main()
 {
-    int height, max_idx = 0, row_nodes, idx = 0;
-    pair<int, int> nodes_per_level[height + 1];
+    int height, max_idx = 0, row_nodes, idx = 0, tmp;
+    pair<int, pair<int, int>> nodes_per_level[height + 1];
     string path;
     cin >> height >> path;
+    row_nodes = pow(2, height);
 
     for (int i = height; i >= 0; i--)
     {
-        row_nodes = pow(2, i);
+        tmp = max_idx;
         max_idx += row_nodes;
-        nodes_per_level[i] = make_pair(row_nodes, max_idx);
+        nodes_per_level[i] = make_pair(row_nodes, make_pair(tmp, max_idx));
+        row_nodes >> 1; // not SRL but okay since we're working with positive ints (also powers of 2)
+    }
+
+    for (int i = 0; i <= height; i++)
+    {
+        cout << i << ": " << nodes_per_level[i].first << " " << nodes_per_level[i].second.first << " " << nodes_per_level[i].second.second << endl;
     }
 }
 
