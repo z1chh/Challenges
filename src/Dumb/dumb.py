e819b84e315f6079fe08fb867e30096f8c514417
z1chh <lolchen2012@gmail.com>
2022-10-28 17:24:06 -0400

    hello world

--- src/Dumb/dumb.py
+++ src/Dumb/dumb.py
@@ -1,26 +1,7 @@
-s = "allo"
-for c in s:
-    print(c)
-    
-if False and 1 / 0:
-    print("True")
-    
-print(s[3])
+def main():
+    s = "Hello World"
+    print(s)
 
-print(int(5 / 2))
 
-print(max([1, 2, 3, 4, 5, 6]))
-
-def test():
-    while True:
-        try:
-            print(int(input()))
-        except:
-            break
-
-print(15 * 15 + 9 + 49)
-print(13 * 13 + 49)
-
-l = [1, 2, 3, 4]
-l = l[0:len(l) - 1]
-print(l)
+if __name__ == "__main__":
+    main()
