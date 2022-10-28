8dcc63a7331fb3ebfde66d0a5f53e623f8b3caeb
z1chh <lolchen2012@gmail.com>
2022-10-28 18:25:28 -0400

    why ocaml

--- src/Dumb/test.ml
+++ src/Dumb/test.ml
@@ -1,3 +1,9 @@
-let a = 5
-(* Some code *)
-a;; (*  *)
+(* NOTES *)
+
+let reverse l =
+    let rec reverse_tr l acc =
+        match l with
+        | []   -> acc
+        | h::t -> reverse_tr t h::acc
+    in
+    reverse_tr l []
