7dbaa9e9649ef75682ba90e4068527d7cffea098
z1chh <lolchen2012@gmail.com>
2022-10-29 16:41:32 -0400

    Finished A8 Problem B

--- src/Kattis/Programming Challenges/Assignments/A8/B.cpp
+++ src/Kattis/Programming Challenges/Assignments/A8/B.cpp
@@ -6,14 +6,17 @@ using namespace std;
 
 int main()
 {
+    // Declare variables
     int nb;
     string name;
     while (true)
     {
+        // Get input
         cin >> nb;
         if (nb == 0)
             break;
 
+        // Store names with their 2 first characters
         string names[nb];
         pair<string, int> names_shortened[nb];
         for (int i = 0; i < nb; i++)
@@ -22,11 +25,18 @@ int main()
             names[i] = name;
             names_shortened[i] = make_pair(name.substr(0, 2), i);
         }
+
+        // Sort names by first two characters
         stable_sort(names_shortened, names_shortened + sizeof(names_shortened) / sizeof(names_shortened[0]));
+
+        // Output names
         for (auto n : names_shortened)
         {
             cout << names[n.second] << endl;
         }
         cout << endl;
     }
+
+    // Successful return
+    return 0;
 }
