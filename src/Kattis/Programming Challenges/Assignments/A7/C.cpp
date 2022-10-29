cf8c87ad0377063c8b9066b49b595383e361921c
z1chh <lolchen2012@gmail.com>
2022-10-29 15:05:07 -0400

    WA

--- src/Kattis/Programming Challenges/Assignments/A7/C.cpp
+++ src/Kattis/Programming Challenges/Assignments/A7/C.cpp
@@ -1,138 +1,83 @@
 #include <iostream>
-#include <map>
+#include <queue>
+#include <set>
 #include <vector>
 
 using namespace std;
 
-void update_ratings(map<int, int> *movie_ratings, int ID1, int ID2, int rating1, int rating2, map<int, vector<int>> *similarities);
+int BFS(vector<int> *adjacent_nodes[], set<int> *unvisited_nodes, queue<pair<int, int>> *q);
 
 int main()
 {
     // Get input
-    int n, h, l, best_rating = -1, best_ID = 1001;
+    int n, h, l;
     cin >> n >> h >> l;
-    map<int, vector<int>> similarities;
-
-    // Initialize movie_ratings
-    map<int, int> movie_ratings;
+    vector<int> adjacent_nodes[n];
     for (int i = 0; i < n; i++)
     {
-        movie_ratings[i] = INT_MAX;
+        adjacent_nodes[i] = vector<int>();
     }
 
-    // Get horror list
+    set<int> unvisited_nodes = set<int>();
+    for (int i = 0; i < n; i++)
+    {
+        unvisited_nodes.insert(i);
+    }
+
+    queue<pair<int, int>> q = queue<pair<int, int>>();
+
     int horror_movie_ID;
     for (int i = 0; i < h; i++)
     {
         cin >> horror_movie_ID;
-        movie_ratings[horror_movie_ID] = 0;
+        unvisited_nodes.erase(horror_movie_ID);
+        q.push(make_pair(0, horror_movie_ID));
     }
 
-    int ID1, ID2, rating1, rating2;
+    int u, v;
     for (int i = 0; i < l; i++)
     {
-        cin >> ID1 >> ID2;
-
-        // Update similar movies
-        similarities[ID1].push_back(ID2);
-        similarities[ID2].push_back(ID1);
-
-        // Get ratings
-        if (movie_ratings.count(ID1) == 1)
-        {
-            rating1 = movie_ratings[ID1];
-        }
-        else
-        {
-            rating1 = -1;
-        }
-        if (movie_ratings.count(ID2) == 1)
-        {
-            rating2 = movie_ratings[ID2];
-        }
-        else
-        {
-            rating2 = -1;
-        }
-        update_ratings(&movie_ratings, ID1, ID2, rating1, rating2, &similarities);
-        /* cout << "IDS " << ID1 << " " << ID2 << endl;
-        for (auto r : movie_ratings)
-        {
-            cout << r.first << " has value " << r.second << endl;
-        } */
+        cin >> u >> v;
+        adjacent_nodes[u].push_back(v);
+        adjacent_nodes[v].push_back(u);
     }
 
-    for (int i = 0; i < n; i++)
+    int c = -1, i = -1, cost, x;
+    while (q.size() > 0)
     {
-        int cur_rating = movie_ratings[i];
-        if (cur_rating > best_rating)
+        // Pop from queue
+        cost = q.front().first;
+        x = q.front().second;
+        q.pop();
+
+        if (cost > c || (cost == c && x < i))
         {
-            best_rating = cur_rating;
-            best_ID = i;
+            c = cost;
+            i = x;
         }
-        else if (cur_rating == best_rating)
+        for (int u : adjacent_nodes[x])
         {
-            if (i < best_ID)
+            if (unvisited_nodes.find(u) != unvisited_nodes.end())
             {
-                best_ID = i;
+                unvisited_nodes.erase(u);
+                q.push(make_pair(cost + 1, u));
             }
         }
     }
-
-    // Output movie with highest Horror Index
-    cout << best_ID << endl;
+    if (unvisited_nodes.size() != 0)
+    {
+        cout << i << endl;
+    }
+    else
+    {
+        int smallest = INT_MAX;
+        for (int n : unvisited_nodes)
+        {
+            smallest = min(smallest, n);
+        }
+        cout << smallest << endl;
+    }
 
     // Successful return
     return 0;
-}
-
-void update_ratings(map<int, int> *movie_ratings, int ID1, int ID2, int rating1, int rating2, map<int, vector<int>> *similarities)
-{
-    if (rating1 == 0 && rating2 != 0)
-    {
-        (*movie_ratings)[ID2] = 1;
-        for (int i = 0; i < (*similarities)[ID2].size(); i++)
-        {
-            if ((*similarities)[ID2][i] > 2)
-            {
-                update_ratings(movie_ratings, ID2, (*similarities)[ID2][i], (*movie_ratings)[ID2], (*movie_ratings)[(*similarities)[ID2][i]], similarities);
-            }
-        }
-    }
-    else if (rating2 == 0 && rating1 != 0)
-    {
-        (*movie_ratings)[ID1] = 1;
-        for (int i = 0; i < (*similarities)[ID1].size(); i++)
-        {
-            if ((*similarities)[ID1][i] > 2)
-            {
-                update_ratings(movie_ratings, ID1, (*similarities)[ID1][i], (*movie_ratings)[ID1], (*movie_ratings)[(*similarities)[ID1][i]], similarities);
-            }
-        }
-    }
-    else if (rating2 != 0 && rating1 != 0)
-    {
-        if (rating1 < rating2)
-        {
-            (*movie_ratings)[ID2] = (*movie_ratings)[ID1] + 1;
-            for (int i = 0; i < (*similarities)[ID2].size(); i++)
-            {
-                if ((*similarities)[ID2][i] > 2)
-                {
-                    update_ratings(movie_ratings, ID2, (*similarities)[ID2][i], (*movie_ratings)[ID2], (*movie_ratings)[(*similarities)[ID2][i]], similarities);
-                }
-            }
-        }
-        else if (rating1 > rating2)
-        {
-            (*movie_ratings)[ID1] = (*movie_ratings)[ID2] + 1;
-            for (int i = 0; i < (*similarities)[ID2].size(); i++)
-            {
-                if ((*similarities)[ID2][i] > 2)
-                {
-                    update_ratings(movie_ratings, ID2, (*similarities)[ID2][i], (*movie_ratings)[ID2], (*movie_ratings)[(*similarities)[ID2][i]], similarities);
-                }
-            }
-        }
-    }
 }
