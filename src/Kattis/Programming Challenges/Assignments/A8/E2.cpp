310b53e6ce1102f5b541ffaaa1b054adfa56d398
z1chh <lolchen2012@gmail.com>
2022-10-29 16:09:29 -0400

    Still TLE

---
+++ src/Kattis/Programming Challenges/Assignments/A8/E2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string pattern, text;
+    int pattern_size, text_size, position, index_str;
+    while (getline(std::cin, pattern))
+    {
+        getline(std::cin, text);
+        pattern_size = pattern.size();
+        text_size = text.size();
+        position = 0;
+
+        while (true)
+        {
+            index_str = text.find(pattern, position);
+            if (index_str != string::npos)
+            {
+                cout << index_str << " ";
+                position = index_str;
+            }
+            if (++position == text_size)
+            {
+                break;
+            }
+        }
+        cout << endl;
+    }
+}
