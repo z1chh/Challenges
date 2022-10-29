c1665fea8a97d7fafb0b623ddcde340982a9a33a
z1chh <lolchen2012@gmail.com>
2022-10-29 16:39:41 -0400

    TLE

---
+++ src/Kattis/Programming Challenges/Assignments/A8/E3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string pattern, text;
+    int pattern_size, text_size;
+    while (getline(std::cin, pattern))
+    {
+        getline(std::cin, text);
+        pattern_size = pattern.size();
+        text_size = text.size();
+        /* cout << pattern_size << " " << text_size << endl;
+        cout << "Iterating from i = 0 to " << text_size - pattern_size << endl; */
+        for (int i = 0; i < text_size - pattern_size + 1; i++)
+        {
+            /* cout << "Index " << i << ": " << pattern << " " << text.substr(i, pattern_size) << endl; */
+            if (pattern == text.substr(i, pattern_size))
+            {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    // Successful return
+    return 0;
+}
