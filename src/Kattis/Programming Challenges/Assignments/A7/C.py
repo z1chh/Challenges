ef7f77ed8b70ed68c332a3292376998be259d61f
z1chh <lolchen2012@gmail.com>
2022-10-29 14:34:38 -0400

    Finished A7 Problem C

--- src/Kattis/Programming Challenges/Assignments/A7/C.py
+++ src/Kattis/Programming Challenges/Assignments/A7/C.py
@@ -1,46 +1,44 @@
+from collections import deque
+
+
+def BFS(adjacent_nodes, unvisited_nodes, queue):
+    c, i = -1, -1
+    while queue:
+        cost, v = queue.popleft()
+        if cost > c or (cost == c and v < i):
+            c, i = cost, v
+        for u in adjacent_nodes[v]:
+            if u in unvisited_nodes:
+                unvisited_nodes.remove(u)
+                queue.append((cost + 1, u))
+    if not unvisited_nodes:
+        return i
+    else:
+        return min(unvisited_nodes)
+
+
+# Get input
 n, h, l = map(int, input().split())
-default_horrors = list(map(int, input().split()))
-movie_ratings = {}
-for id in default_horrors:
-    movie_ratings[id] = 0
-to_do = []
+
+# Create new graph
+adjacent_nodes = []
+for _ in range(n):
+    adjacent_nodes.append([])
+unvisited_nodes = set()
+for i in range(n):
+    unvisited_nodes.add(i)
+
+# Create queue for nodes to visit during BFS
+queue = deque()
+
+# Populate graph
+for s in map(int, input().split()):
+    unvisited_nodes.remove(s)
+    queue.append((0, s))
 for _ in range(l):
-    id1, id2 = map(int, input().split())
-    if id1 in movie_ratings:
-        if id2 in movie_ratings:
-            if movie_ratings[id1] < movie_ratings[id2]:
-                movie_ratings[id2] = movie_ratings[id1] + 1
-            elif movie_ratings[id2] < movie_ratings[id1]:
-                movie_ratings[id1] = movie_ratings[id2] + 1
-        else:
-            movie_ratings[id2] = movie_ratings[id1] + 1
-    else:
-        if id2 in movie_ratings:
-            movie_ratings[id1] = movie_ratings[id2] + 1
-        else:
-            to_do.append((id1, id2))
-while to_do:
-    id1, id2 = to_do.pop(0)
-    if id1 in movie_ratings:
-        if id2 in movie_ratings:
-            if movie_ratings[id1] < movie_ratings[id2]:
-                movie_ratings[id2] = movie_ratings[id1] + 1
-            elif movie_ratings[id2] < movie_ratings[id1]:
-                movie_ratings[id1] = movie_ratings[id2] + 1
-        else:
-            movie_ratings[id2] = movie_ratings[id1] + 1
-    else:
-        if id2 in movie_ratings:
-            movie_ratings[id1] = movie_ratings[id2] + 1
-        else:
-            to_do.append((id1, id2))
-id = n
-lowest = 0
-for k, v in movie_ratings.items():
-    if v > lowest:
-        lowest = v
-        id = k
-    elif v == lowest:
-        if k < id:
-            id = k
-print(id)
+    u, v = map(int, input().split())
+    adjacent_nodes[v].append(u)
+    adjacent_nodes[u].append(v)
+
+# Compute BFS and output result
+print(BFS(adjacent_nodes, unvisited_nodes, queue))
