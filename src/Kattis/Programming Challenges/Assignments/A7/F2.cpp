34b2794fbcfcbc39f17f3f39d0db1906663d0962
z1chh <lolchen2012@gmail.com>
2022-10-29 00:53:05 -0400

    get input items

--- src/Kattis/Programming Challenges/Assignments/A7/F2.cpp
+++ src/Kattis/Programming Challenges/Assignments/A7/F2.cpp
@@ -8,5 +8,19 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    int items, pairs;
+    string s;
+    cin >> items;
+    map<string, set<string>> ingredients;
+    set<string> a;
+    set<string> b;
+    bool possible = true;
+    vector<string> v;
+
+    for (int i = 0; i < items; i++)
+    {
+        cin >> s;
+        ingredients[s] = set<string>();
+        v.push_back(s);
+    }
 }
