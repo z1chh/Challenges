b04b8f36266d3a25bc18d427c2f393c0fbe4219e
z1chh <lolchen2012@gmail.com>
2022-10-29 16:09:23 -0400

    How to substr

--- src/Kattis/Programming Challenges/Assignments/A8/test.cpp
+++ src/Kattis/Programming Challenges/Assignments/A8/test.cpp
@@ -8,5 +8,10 @@ int main()
     string s = "0123456789";
     string s2 = s.substr(1, 3);
     cout << s2 << endl;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << s.substr(i, i) << endl;
+    }
     return 0;
 }
