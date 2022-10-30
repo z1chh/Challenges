e5695b9f65569cbd7acb7ca9baf845fd124940f3
z1chh <lolchen2012@gmail.com>
2022-10-30 15:05:55 -0400

    Problem 1

---
+++ src/Tech Questions/Roblox/a.py
@@ -0,0 +1,28 @@
+def solution(numbers):
+    ct = 0
+    d = {}
+    s = set()
+    for num in numbers:
+        s.add(num)
+        if num in d:
+            d[num] += 1
+        else:
+            d[num] = 1
+
+    s = list(s)
+    for i in range(len(s)):
+        for j in range(i + 1, len(s)):
+            n1 = str(s[i])
+            n2 = str(s[j])
+            if len(n1) != len(n2):
+                continue
+            else:
+                diff = 0
+                for c1, c2 in zip(n1, n2):
+                    if c1 != c2:
+                        diff += 1
+                        if diff > 1:
+                            break
+                if diff == 1:
+                    ct += d[s[i]] * d[s[j]]
+    return c
