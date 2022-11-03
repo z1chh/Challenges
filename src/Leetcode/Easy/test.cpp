f81b977a1fe6115ccb21b73095f4d55040d726c9
z1chh <lolchen2012@gmail.com>
2022-11-03 00:07:15 -0400

    test

--- src/Leetcode/Easy/test.cpp
+++ src/Leetcode/Easy/test.cpp
@@ -43,21 +43,16 @@ void t1()
 void t2()
 {
     vector<vector<int>> v;
-    vector<int> a;
-    a.push_back(1);
-    a.push_back(2);
+    vector<int> a = {1, 2};
     v.push_back(a);
     a.clear();
-    a.push_back(2);
-    a.push_back(1);
+    a = {2, 1};
     v.push_back(a);
     a.clear();
-    a.push_back(5);
-    a.push_back(2);
+    a = {5, 2};
     v.push_back(a);
     a.clear();
-    a.push_back(2);
-    a.push_back(5);
+    a = {2, 5};
     v.push_back(a);
 
     for (int i = 0; i < v.size(); i++)
