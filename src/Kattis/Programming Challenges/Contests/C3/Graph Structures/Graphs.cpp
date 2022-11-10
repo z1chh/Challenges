cc0b2623c99597084ab3a148af948efc7b97f0f4
z1chh <lolchen2012@gmail.com>
2022-11-10 00:34:03 -0500

    Graphs

---
+++ src/Kattis/Programming Challenges/Contests/C3/Graph Structures/Graphs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class MatrixGraph
+{
+public:
+    // Attributes
+    int *matrix;
+
+    // Constructor
+    MatrixGraph();
+
+    // Destructor
+    ~MatrixGraph();
+
+    // Other methods
+    // TODO: Implement
+};
