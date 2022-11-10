bfaafc26b3d4d7d7355f1c044f8ba176ec128a87
z1chh <lolchen2012@gmail.com>
2022-11-10 00:39:47 -0500

    Undirected graph

--- src/Kattis/Programming Challenges/Contests/C3/Graph Structures/Graphs.cpp
+++ src/Kattis/Programming Challenges/Contests/C3/Graph Structures/Graphs.cpp
@@ -6,14 +6,52 @@ class MatrixGraph
 {
 public:
     // Attributes
-    int *matrix;
+    int numNodes;
+    int **matrix;
 
     // Constructor
-    MatrixGraph();
+    MatrixGraph(int totalNodes);
 
     // Destructor
     ~MatrixGraph();
 
     // Other methods
-    // TODO: Implement
+    bool addEdge(int node1, int node2);
 };
+
+MatrixGraph::MatrixGraph(int totalNodes)
+{
+    this->numNodes = totalNodes;
+    this->matrix = new int[totalNodes][totalNodes];
+    for (int i = 0; i < this->numNodes; i++)
+    {
+        this->matrix[i] = new int[totalNodes];
+    }
+}
+
+MatrixGraph::~MatrixGraph()
+{
+    for (int i = 0; i < this->numNodes; i++)
+    {
+        delete[] this->matrix[i];
+    }
+    delete[] this->matrix;
+}
+
+bool MatrixGraph::addEdge(int node1, int node2)
+{
+    if (this->matrix[node1][node2] != 0)
+    {
+        return false;
+    }
+    else
+    {
+        this->matrix[node1][node2] = 1;
+        return true;
+    }
+}
+
+int main()
+{
+    MatrixGraph m = MatrixGraph(5);
+}
