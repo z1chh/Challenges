43ce7256eb33e6851bea8aa943c8ea398dbc6414
z1chh <lolchen2012@gmail.com>
2022-11-11 18:52:28 -0500

    Contest 3 Problem A: I Repeat Myself

--- src/Kattis/Programming Challenges/Contests/C3/A.cpp
+++ src/Kattis/Programming Challenges/Contests/C3/A.cpp
@@ -1,12 +1,66 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    // Get input
-    // TODO
+    // Get number of cases
+    int cases;
+    cin >> cases;
 
-    // Successful return
+    int rep, size, idx;
+    string inp, pattern, substr;
+
+    // Flush buffer
+    getline(cin, pattern);
+
+    for (int x = 0; x < cases; x++)
+    {
+        // Get input
+        getline(cin, inp);
+        size = inp.size();
+
+        // Reset vars
+        rep = -1;
+
+        for (int i = 1; i < size; i++)
+        {
+            pattern = inp.substr(0, i);
+            idx = i;
+            while (idx < size)
+            {
+                if (idx + i > size)
+                {
+                    substr = inp.substr(idx, size - idx);
+                    pattern = pattern.substr(0, size - idx);
+                    idx = size;
+                }
+                else
+                {
+                    substr = inp.substr(idx, i);
+                    idx += i;
+                }
+                if (pattern != substr)
+                    break;
+            }
+
+            if (pattern == substr)
+            {
+                rep = i;
+                break;
+            }
+        }
+
+        // Output answer
+        if (rep == -1)
+        {
+            cout << size << endl;
+        }
+        else
+        {
+            cout << rep << endl;
+        }
+    }
     return 0;
 }
