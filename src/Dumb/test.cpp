6e6d0a5fa7ecf60ee1f27c067295c262e31bb390
z1chh <lolchen2012@gmail.com>
2022-11-11 20:44:41 -0500

    testing

--- src/Dumb/test.cpp
+++ src/Dumb/test.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 int main()
 {
+    float f1 = 1., f2 = 2., f3 = 3., f4;
+    f4 = max(f1, f2);
+    f4 = max(f4, f3);
+    cout << f4 << endl;
     int x;
     string s = "HI";
     cin >> x;
