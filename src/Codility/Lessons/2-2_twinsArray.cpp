1ec22416e032de1e7f34ed8f8a959953216107b9
z1chh <lolchen2012@gmail.com>
2022-11-12 12:12:52 -0500

    Unity Codility Lesson 2

---
+++ src/Codility/Lessons/2-2_twinsArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A)
+{
+    // write your code in C++14 (g++ 6.2.0)
+    set<int> s;
+    for (int n: A)
+    {
+        // IF IN S REMOVE IT, ELSE ADD IT
+    }
+
+    // ODD NUMBER IS THE ONLY LEFT IN SET
+    // I GTG SO SUBMIT THIS BUT JUST TO SHOW I ALREADY HAVE THE ANSWER
+}
