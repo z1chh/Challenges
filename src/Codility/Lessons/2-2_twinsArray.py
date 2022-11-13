65fd1fb8bc9003fd716350fb21e62ff5e1c39a4a
z1chh <lolchen2012@gmail.com>
2022-11-12 23:58:39 -0500

    Unity Codility Lesson 2 (Python)

---
+++ src/Codility/Lessons/2-2_twinsArray.py
@@ -0,0 +1,14 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.8.10
+    s = set()
+    for el in A:
+        if el in s:
+            s.remove(el)
+        else:
+            s.add(el)
+    for el in s:
+        return el
+
