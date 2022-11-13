94e0bc5fdcc9751eddd6220fc2cceadea14abbe9
z1chh <lolchen2012@gmail.com>
2022-11-13 12:34:48 -0500

    Unity Codility Lesson 3-1 (Python)

---
+++ src/Codility/Lessons/3-1_fromJump.py
@@ -0,0 +1,7 @@
+import math
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(X, Y, D):
+    # write your code in Python 3.8.10
+    return math.ceil((Y - X) / D)
