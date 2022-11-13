124e41bcb6e34d3577fb8d9a5ff2870dd690f1b5
z1chh <lolchen2012@gmail.com>
2022-11-13 14:12:32 -0500

    Improved runtime from O(n^2) to O(n)

--- src/Codility/Lessons/3-3_splitArrayDifference.py
+++ src/Codility/Lessons/3-3_splitArrayDifference.py
@@ -13,6 +13,42 @@ def n2(A):
             best = abs(cur)
     return best
 
+def better(A):
+    # Get size of array
+    size = len(A)
+    
+    # Base cases
+    if size == 0:
+        return 0
+    elif size == 1:
+        return A[0]
+    elif size == 2:
+        return abs(A[0] - A[1])
+    elif size == 3:
+        return abs(A[0] - A[1] - A[2])
+    
+    # Store sums from left and from right
+    sumLeft = [A[0]]
+    for i in range(size - 1):
+        sumLeft.append(sumLeft[i] + A[i + 1])
+    
+    sumRight = [A[-1]]
+    for i in range(size - 1):
+        sumRight.append(sumRight[i] + A[-2 - i])
+    
+    # Get best split
+    best = 2000000000
+    for i in range(size - 1):
+        if abs(sumLeft[i] - sumRight[-2 - i]) < best:
+            best = abs(sumLeft[i] - sumRight[-2 - i])
+    return best
+
 def solution(A):
     # write your code in Python 3.8.10
-    return n2(A)
+    return better(A)
+
+def main():
+    print(solution([3, 1, 2, 4, 3]))
+
+if __name__ == "__main__":
+    main()
