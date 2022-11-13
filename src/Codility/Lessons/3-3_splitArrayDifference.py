3b7f31a9f12dc82c343600e6d8e5cf15c856df28
z1chh <lolchen2012@gmail.com>
2022-11-13 14:20:43 -0500

    Unity Codility Lesson 3-3 (92%)

--- src/Codility/Lessons/3-3_splitArrayDifference.py
+++ src/Codility/Lessons/3-3_splitArrayDifference.py
@@ -1,19 +1,7 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-def n2(A):
-    best = 2000000000
-    for P in range(1, len(A)):
-        cur = 0
-        for el in A[:P]:
-            cur += el
-        for el in A[P:]:
-            cur -= el
-        if abs(cur) < best:
-            best = abs(cur)
-    return best
-
-def better(A):
+def solution(A):
     # Get size of array
     size = len(A)
     
@@ -29,26 +17,16 @@ def better(A):
     
     # Store sums from left and from right
     sumLeft = [A[0]]
-    for i in range(size - 1):
+    for i in range(size - 2):
         sumLeft.append(sumLeft[i] + A[i + 1])
     
     sumRight = [A[-1]]
-    for i in range(size - 1):
+    for i in range(size - 2):
         sumRight.append(sumRight[i] + A[-2 - i])
     
     # Get best split
     best = 2000000000
     for i in range(size - 1):
-        if abs(sumLeft[i] - sumRight[-2 - i]) < best:
-            best = abs(sumLeft[i] - sumRight[-2 - i])
+        if (cur := abs(sumLeft[i] - sumRight[-1 - i])) < best:
+            best = cur
     return best
-
-def solution(A):
-    # write your code in Python 3.8.10
-    return better(A)
-
-def main():
-    print(solution([3, 1, 2, 4, 3]))
-
-if __name__ == "__main__":
-    main()
