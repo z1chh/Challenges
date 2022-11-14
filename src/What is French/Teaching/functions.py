a5715f14c3b3f642b3aff94e14aa4ac082120253
z1chh <lolchen2012@gmail.com>
2022-11-14 00:21:16 -0500

    Dumb brother

--- src/What is French/Teaching/functions.py
+++ src/What is French/Teaching/functions.py
@@ -1,9 +1,15 @@
 def increment(x):
     print(f"Incrementing {x} by 1.")
     return x + 1
-a = 5
-b = increment(a)
-increment(10)
-print(a)
-print(b)
-print(increment(a))
+
+def main():
+    a = 5
+    b = increment(a)
+    increment(10)
+    print(a)
+    print(increment(14))
+    print(b)
+    print(increment(a))
+
+if __name__ == "__main__":
+    main()
