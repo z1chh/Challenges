2fdbc53493222abe5bee9333e22ede415d29215c
z1chh <lolchen2012@gmail.com>
2022-11-14 11:39:31 -0500

    TODO: Compute number of items

--- src/Kattis/Programming Challenges/Assignments/A10/B.py
+++ src/Kattis/Programming Challenges/Assignments/A10/B.py
@@ -19,49 +19,27 @@ class Graph():
         for node in range(self.V):
             print(node, "\t", dist[node])
 
-    # A utility function to find the vertex with
-    # minimum distance value, from the set of vertices
-    # not yet included in shortest path tree
     def minDistance(self, dist, sptSet):
         # Initialize min_index to -1
         min_index = -1
         # Initialize minimum distance for next node
         min = sys.maxsize
 
-        # Search not nearest vertex not in the
-        # shortest path tree
+        # Search not nearest vertex not in the shortest path tree
         for u in range(self.V):
             if dist[u] < min and sptSet[u] == False:
                 min = dist[u]
                 min_index = u
-
         return min_index
 
-    # Function that implements Dijkstra's single source
-    # shortest path algorithm for a graph represented
-    # using adjacency matrix representation
     def dijkstra(self, src):
         self.start = src
-
         dist = [sys.maxsize] * self.V
         dist[src] = 0
         sptSet = [False] * self.V
-
-        for cout in range(self.V):
-
-            # Pick the minimum distance vertex from
-            # the set of vertices not yet processed.
-            # x is always equal to src in first iteration
+        for _ in range(self.V):
             x = self.minDistance(dist, sptSet)
-
-            # Put the minimum distance vertex in the
-            # shortest path tree
             sptSet[x] = True
-
-            # Update dist value of the adjacent vertices
-            # of the picked vertex only if the current
-            # distance is greater than new distance and
-            # the vertex in not in the shortest path tree
             for y in range(self.V):
                 if self.graph[x][y] > 0 and sptSet[y] == False and \
                         dist[y] > dist[x] + self.graph[x][y]:
