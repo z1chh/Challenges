cd40f7b116ec7c0adf555fdfecf60aa33a4f3eaa
z1chh <lolchen2012@gmail.com>
2022-11-16 01:15:06 -0500

    English

--- src/What is French/Paint/Paint.py
+++ src/What is French/Paint/Paint.py
@@ -36,9 +36,10 @@ def setPixel(x, y, c):
 
 WIDTH = 180
 HEIGHT = 120
+DEFAULTSTART = 6
 ERASECOLOR = "#fff"
 COLOR = "#fff"
-IMAGE = [] # codeBoot does not support list comprehension :D
+IMAGE = []  # codeBoot does not support list comprehension :D
 for _ in range(WIDTH):
     lst = list()
     for _ in range(HEIGHT):
@@ -46,66 +47,68 @@ for _ in range(WIDTH):
     IMAGE.append(lst)
 SIZE = math.floor(HEIGHT / 10)
 SPACING = math.floor(SIZE / 2)
-COLORS = ["#fff", "#000", "#f00", "#ff0", "#0f0", "#00f", "#f0f", "#888"] # White, black, red, yellow, green, blue, fuschia, grey
+# White, black, red, yellow, green, blue, fuschia, grey
+COLORS = ["#fff", "#000", "#f00", "#ff0", "#0f0", "#00f", "#f0f", "#888"]
 
 
-def createBoutons(couleurs, taille, espace, couleurEffacer):
-    y2 = 6 + taille
-    effacer = struct(coin1=struct(x=6, y=6),
-                     coin2=struct(x=6 + taille, y=y2),
-                     couleur=couleurEffacer,
-                     effacer=True)
-    x1 = 6 + taille + espace
-    boutons = [effacer]
-    for c in couleurs:
-        bouton = struct(coin1=struct(x=x1, y=6),
-                        coin2=struct(x=x1 + taille, y=y2),
-                        couleur=c,
-                        effacer=False)
-        boutons.append(bouton)
-        x1 += taille + espace
-    return boutons
+def createButtons(colors, size, spacing, eraseColor):
+    y2 = DEFAULTSTART + size
+    eraseButton = struct(corner1=struct(x=DEFAULTSTART, y=DEFAULTSTART),
+                         corner2=struct(x=DEFAULTSTART + size, y=y2),
+                         color=eraseColor,
+                         erase=True)
+    x1 = DEFAULTSTART + size + spacing
+    buttons = [eraseButton]
+    for c in colors:
+        button = struct(corner1=struct(x=x1, y=DEFAULTSTART),
+                        corner2=struct(x=x1 + size, y=y2),
+                        color=c,
+                        erase=False)
+        buttons.append(button)
+        x1 += size + spacing
+    return buttons
 
 
-def findBouton(boutons, position):
-    for bouton in boutons:
-        if position.x >= bouton.coin1.x and position.x <= bouton.coin2.x:
-            if position.y >= bouton.coin1.y and position.y <= bouton.coin2.y:
-                return bouton
+def findButton(buttons, position):
+    for button in buttons:
+        if position.x >= button.corner1.x and position.x <= button.corner2.x:
+            if position.y >= button.corner1.y and position.y <= button.corner2.y:
+                return button
     return None
 
 
-def drawFloatingRectangle(imageOriginale, debut, couleur):
-    xDebut = debut.x
-    yDebut = debut.y
-    xFinal = xDebut
-    yFinal = yDebut
-    x1 = xDebut
-    x2 = xDebut
-    y1 = yDebut
-    y2 = yDebut
+def drawFloatingRectangle(originalImage, start, color):
+    xStart = start.x
+    yStart = start.y
+    xFinal = xStart
+    yFinal = yStart
+    x1 = xStart
+    x2 = xStart
+    y1 = yStart
+    y2 = yStart
     while True:
         # Check if continue to draw floating rectangle
         mouse = getMouse()
         x = mouse.x
         y = mouse.y
-        boutton = mouse.button
-        if boutton == 0:
+        button = mouse.button
+        if button == 0:
             break
 
         # Reset to original grid
-        restoreImage(imageOriginale, struct(coin1=struct(
-            x=x1, y=y1), coin2=struct(x=x2, y=y2)))
+        restoreImage(originalImage,
+                     struct(corner1=struct(x=x1, y=y1),
+                            corner2=struct(x=x2, y=y2)))
 
         # Get new floating rectangle
-        x1 = min(x, debut.x)
-        x2 = max(x, debut.x)
-        y1 = min(y, debut.y)
-        y2 = max(y, debut.y)
+        x1 = min(x, start.x)
+        x2 = max(x, start.x)
+        y1 = min(y, start.y)
+        y2 = max(y, start.y)
 
         # Color
-        y1 = max(y1, math.ceil(HAUTEUR / 5))
-        fillRectangle(x1, y1, x2 - x1, y2 - y1, couleur)
+        y1 = max(y1, math.ceil(HEIGHT / 5))
+        fillRectangle(x1, y1, x2 - x1, y2 - y1, color)
 
         # Update last coords
         xFinal = x
@@ -115,42 +118,42 @@ def drawFloatingRectangle(imageOriginale, debut, couleur):
         sleep(0.01)
 
     # Update the original image
-    rectangle = struct(coin1=struct(x=min(xDebut, xFinal), y=min(
-        yDebut, yFinal)), coin2=struct(x=max(xDebut, xFinal) + 1, y=max(yDebut, yFinal) + 1))
-    addRectangle(imageOriginale, rectangle, couleur)
+    rectangle = struct(corner1=struct(x=min(xStart, xFinal), y=min(yStart, yFinal)),
+                       corner2=struct(x=max(xStart, xFinal) + 1, y=max(yStart, yFinal) + 1))
+    addRectangle(originalImage, rectangle, color)
     return
 
 
-def restoreImage(imageOriginale, rectangle):
-    for i in range(rectangle.coin1.x, rectangle.coin2.x - 1):
-        for j in range(max(math.ceil(HAUTEUR / 5), rectangle.coin1.y), rectangle.coin2.y - 1):
-            setPixel(i, j, imageOriginale[i][j])
+def restoreImage(originalImage, rectangle):
+    for i in range(rectangle.corner1.x, rectangle.corner2.x):
+        for j in range(max(math.ceil(HEIGHT / 5), rectangle.corner1.y), rectangle.corner2.y):
+            setPixel(i, j, originalImage[i][j])
     return
 
 
-def addRectangle(image, rectangle, couleur):
-    for i in range(rectangle.coin1.x, rectangle.coin2.x):
-        for j in range(max(math.ceil(HAUTEUR / 5), rectangle.coin1.y), rectangle.coin2.y):
-            image[i][j] = couleur
+def addRectangle(image, rectangle, color):
+    for i in range(rectangle.corner1.x, rectangle.corner2.x):
+        for j in range(max(math.ceil(HEIGHT / 5), rectangle.corner1.y), rectangle.corner2.y):
+            image[i][j] = color
     return
 
 
-def treatNextClick(boutons):
-    COULEUR = "#fff"
+def treatNextClick(buttons):
+    COLOR = "#fff"
     while True:
         # Check if user clicked
         mouse = getMouse()
         x = mouse.x
         y = mouse.y
-        boutton = mouse.button
-        if boutton == 1:
-            if y <= math.ceil(HAUTEUR / 5):
-                b = findButton(boutons, struct(x=x, y=y))
+        button = mouse.button
+        if button == 1:
+            if y <= math.ceil(HEIGHT / 5):
+                b = findButton(buttons, struct(x=x, y=y))
                 if b is not None:
                     if b.erase:
                         draw()
                     else:
-                        COULEUR = b.color
+                        COLOR = b.color
             else:
                 drawFloatingRectangle(IMAGE, struct(x=x, y=y), COLOR)
         sleep(0.01)
@@ -158,14 +161,14 @@ def treatNextClick(boutons):
 
 def addBorder(button):
     # Horizontal borders
-    for i in range(button.coin1.x, button.coin1.x + SIZE + 1):
-        setPixel(i, button.coin1.y, "#000")
-        setPixel(i, button.coin1.y + SIZE, "#000")
+    for i in range(button.corner1.x, button.corner1.x + SIZE + 1):
+        setPixel(i, button.corner1.y, "#000")
+        setPixel(i, button.corner1.y + SIZE, "#000")
 
     # Vertical borders
-    for i in range(button.coin1.y, button.coin1.y + SIZE):
-        setPixel(button.coin1.x, i, "#000")
-        setPixel(button.coin1.x + SIZE, i, "#000")
+    for i in range(button.corner1.y, button.corner1.y + SIZE):
+        setPixel(button.corner1.x, i, "#000")
+        setPixel(button.corner1.x + SIZE, i, "#000")
 
 
 def draw():
@@ -173,32 +176,32 @@ def draw():
     setScreenMode(WIDTH, HEIGHT)
 
     # Background
-    fillRectangle(0, 0, LARGEUR, HAUTEUR, COULEUREFFACER)
-    fillRectangle(0, 0, LARGEUR, math.ceil(HAUTEUR / 5), "#888")
+    fillRectangle(0, 0, WIDTH, HEIGHT, ERASECOLOR)
+    fillRectangle(0, 0, WIDTH, math.ceil(HEIGHT / 5), "#888")
 
     # Buttons
-    buttons = createButtons(COULEURS, TAILLE, ESPACE, COULEUREFFACER)
+    buttons = createButtons(COLORS, SIZE, SPACING, ERASECOLOR)
     for button in buttons:
-        fillRectangle(button.coin1.x,
-                      button.coin1.y,
-                      button.coin2.x - button.coin1.x,
-                      button.coin2.y - button.coin1.y,
-                      button.couleur)
+        fillRectangle(button.corner1.x,
+                      button.corner1.y,
+                      button.corner2.x - button.corner1.x,
+                      button.corner2.y - button.corner1.y,
+                      button.color)
 
     # Add "X" on erase
-    for i in range(6, 6 + TAILLE):
+    for i in range(6, 6 + SIZE):
         setPixel(i, i, "#f00")
-    for i in range(0, TAILLE):
-        setPixel(6 + TAILLE - i, i + 6, "#f00")
+    for i in range(0, SIZE):
+        setPixel(6 + SIZE - i, i + 6, "#f00")
 
     # Add border for buttons
     for button in buttons:
         addBorder(button)
 
     # Reset IMAGE
-    for i in range(LARGEUR):
-        for j in range(HAUTEUR):
-            IMAGE[i][j] = COULEUREFFACER
+    for i in range(WIDTH):
+        for j in range(HEIGHT):
+            IMAGE[i][j] = ERASECOLOR
 
     # Start program
     # Infinite loop, does not terminate (PDF does not say to terminate)
