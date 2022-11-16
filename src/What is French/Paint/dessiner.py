479993b9342b20192822035804569374feb8ea04
z1chh <lolchen2012@gmail.com>
2022-11-15 22:40:34 -0500

    FINISHED

--- src/What is French/Paint/dessiner.py
+++ src/What is French/Paint/dessiner.py
@@ -22,7 +22,6 @@ def setPixel(x, y, c):
     return
 
 
-
 LARGEUR = 180
 HAUTEUR = 120
 COULEUREFFACER = "#fff"
@@ -36,11 +35,6 @@ for _ in range(LARGEUR):
 TAILLE = 12
 ESPACE = 6
 COULEURS = ["#fff", "#000", "#f00", "#ff0", "#0f0", "#00f", "#f0f", "#888"]
-
-
-"""
-Functions
-"""
 
 
 def creerBoutons(couleurs, taille, espace, couleurEffacer):
@@ -74,14 +68,22 @@ def dessinerRectangleFlottant(imageOriginale, debut, couleur):
     yDebut = debut.y
     xFinal = xDebut
     yFinal = yDebut
+    x1 = xDebut
+    x2 = xDebut
+    y1 = yDebut
+    y2 = yDebut
     while True:
         # Check if continue to draw floating rectangle
         mouse = getMouse()
         x = mouse.x
         y = mouse.y
-        button = mouse.button
-        if button == 0:
+        boutton = mouse.button
+        if boutton == 0:
             break
+
+        # Reset to original grid
+        restaurerImage(imageOriginale, struct(coin1=struct(
+            x=x1, y=y1), coin2=struct(x=x2, y=y2)))
 
         # Get new floating rectangle
         x1 = min(x, debut.x)
@@ -89,11 +91,8 @@ def dessinerRectangleFlottant(imageOriginale, debut, couleur):
         y1 = min(y, debut.y)
         y2 = max(y, debut.y)
 
-        # Reset to original grid
-        restaurerImage(imageOriginale, struct(coin1=struct(
-            x=x1, y=y1), coin2=struct(x=x2, y=y2)))
-        
         # Color
+        y1 = max(y1, math.ceil(HAUTEUR / 5))
         fillRectangle(x1, y1, x2 - x1, y2 - y1, couleur)
 
         # Update last coords
@@ -106,13 +105,13 @@ def dessinerRectangleFlottant(imageOriginale, debut, couleur):
     # Update the original image
     rectangle = struct(coin1=struct(x=min(xDebut, xFinal), y=min(
         yDebut, yFinal)), coin2=struct(x=max(xDebut, xFinal) + 1, y=max(yDebut, yFinal) + 1))
-    restaurerImage(imageOriginale, rectangle)
+    ajouterRectangle(imageOriginale, rectangle, couleur)
     return
 
 
 def restaurerImage(imageOriginale, rectangle):
     for i in range(rectangle.coin1.x, rectangle.coin2.x):
-        for j in range(max(math.ceil(HAUTEUR / 5) + 1, rectangle.coin1.y), rectangle.coin2.y):
+        for j in range(max(math.ceil(HAUTEUR / 5), rectangle.coin1.y), rectangle.coin2.y):
             setPixel(i, j, imageOriginale[i][j])
     return
 
@@ -125,6 +124,7 @@ def ajouterRectangle(image, rectangle, couleur):
 
 
 def traiterProchainClic(boutons):
+    COULEUR = "#fff"
     while True:
         # Check if user clicked
         mouse = getMouse()
@@ -160,18 +160,17 @@ def dessiner():
                       bouton.coin2.x - bouton.coin1.x,
                       bouton.coin2.y - bouton.coin1.y,
                       bouton.couleur)
-    
+
     # Add "X" on erase
     for i in range(6, 6 + TAILLE):
         setPixel(i, i, "#f00")
     for i in range(0, TAILLE):
         setPixel(5 + TAILLE - i, i + 6, "#f00")
-        
+
     # Reset IMAGE
     for i in range(LARGEUR):
         for j in range(HAUTEUR):
             IMAGE[i][j] = COULEUREFFACER
-    
 
     # Start program
     # Infinite loop, does not terminate (PDF does not say to terminate)
