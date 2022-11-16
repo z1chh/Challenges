c8ca41fb97c1e92dd147952295a0fa100090bb48
z1chh <lolchen2012@gmail.com>
2022-11-15 20:21:23 -0500

    dessinerRectangleFlottant

--- src/What is French/Paint/dessiner.py
+++ src/What is French/Paint/dessiner.py
@@ -1,6 +1,28 @@
 import struct
 
 
+"""
+Remove the following functions:
+ - getMouse()
+ - sleep()
+ - fillRectangle(x, y, w, h, c)
+
+These functions are simply declared so that the code compiles, but they are built-in functions from codeBoot
+"""
+
+
+def getMouse():
+    return (0, 0, 0, 0, 0)
+
+
+def sleep(x):
+    return
+
+
+def fillRectangle(x, y, w, h, c):
+    return
+
+
 def creerBoutons(couleurs, taille, espace, couleurEffacer):
     y2 = 6 + taille
     effacer = struct(coin1=struct(x=6, y=6),
@@ -27,5 +49,38 @@ def trouverBouton(boutons, position):
     return None
 
 
-def dessinerRectangleFlottan(imageOriginale, debut, couleur):
-    pass
+def dessinerRectangleFlottant(imageOriginale, debut, couleur):
+    xDebut = debut.x
+    yDebut = debut.y
+    xFinal = xDebut
+    yFinal = yDebut
+    while True:
+        # Check if continue to draw floating rectangle
+        x, y, button, shift, ctrl = getMouse()
+        if button == 0:
+            break
+        
+        # Reset to original grid
+        for i in range(180):
+            for j in range(120):
+                fillRectangle(i, j, 1, 1, imageOriginale[i][j])
+        
+        # Get new floating rectangle
+        x1 = min(x, debut.x)
+        x2 = max(x, debut.x)
+        y1 = min(y, debut.y)
+        y2 = max(y, debut.y)
+        fillRectangle(x1, y1, x2 - x1, y2 - y1, couleur)
+        
+        # Update last coords
+        xFinal = x
+        yFinal = y
+        
+        # Sleep
+        sleep(0.01)
+    
+    # Update the original image
+    for i in range(min(xDebut, xFinal), max(xDebut, xFinal) + 1):
+        for j in range(max(24, min(yDebut, yFinal)), max(yDebut, yFinal) + 1):
+            imageOriginale[i][j] = couleur
+    return
