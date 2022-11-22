b79225e0a617baacc3322a33d83429cbaef46b89
z1chh <lolchen2012@gmail.com>
2022-11-22 00:24:42 -0500

    Started E.cpp

---
+++ src/Kattis/Programming Challenges/Assignments/A11/E.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Get number of cases
+    int num_cases, r, s, x, y, w;
+    cin >> num_cases;
+    for (int i = 0; i < num_cases; i++)
+    {
+        // Get input
+        cin >> r >> s >> x >> y >> w;
+    }
+}
