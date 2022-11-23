93773b43d59e364ad963b12d332e4b6b03863257
z1chh <lolchen2012@gmail.com>
2022-11-23 00:25:16 -0500

    Wrong Answer

--- src/Kattis/Website/Maths/flipfive.py
+++ src/Kattis/Website/Maths/flipfive.py
@@ -8,6 +8,9 @@ Grid:
 | 6 | 7 | 8 |
 +---+---+---+
 """
+
+
+# Tiles to flip in function of the chosen tile
 neighbors = {0: {0, 1, 3},
              1: {0, 1, 2, 4},
              2: {1, 2, 5},
@@ -20,15 +23,21 @@ neighbors = {0: {0, 1, 3},
 
 
 def flipBoard(board, n):
+    # Convert into a list
     newBoard = [c for c in board]
+
+    # Flip the correponding tiles
     for v in neighbors[n]:
         if newBoard[v] == "*":
             newBoard[v] = "."
         else:
             newBoard[v] = "*"
+
+    # Return as string
     return ''.join(newBoard)
 
 
+# Stringify the board (for debugging purposes)
 def boardAsString(board):
     return (f"+---+---+---+\n" +
             f"| {board[0]} | {board[1]} | {board[2]} |\n" +
@@ -58,17 +67,26 @@ for _ in range(int(input())):
     # Exhaustive search
     winScores = set()
     while queue:
+        # Pop the board from the queue and retrieve its score
         poppedBoard = queue.pop(0)
         curScore = storage[poppedBoard]
+
+        # Get every possible move
         for i in range(9):
             flippedBoard = flipBoard(poppedBoard, i)
+
+            # Check if it is a winning board
             if flippedBoard == goalBoard:
                 winScores.add(curScore + 1)
-            elif flippedBoard not in played:
-                queue.append(flippedBoard)
+                # print(boardAsString(poppedBoard))
+                # print(boardAsString(flippedBoard))
+                #print("score of", curScore + 1)
+
+            # Check if not visited already
+            elif flippedBoard not in played or storage[flippedBoard] > curScore + 1:
                 played.add(flippedBoard)
-                if flippedBoard in storage:
-                    storage[flippedBoard] += 1
-                else:
-                    storage[flippedBoard] = 1
+                queue.append(flippedBoard)
+                storage[flippedBoard] = curScore + 1
+
+    # Output minimum number of moves
     print(min(winScores))
