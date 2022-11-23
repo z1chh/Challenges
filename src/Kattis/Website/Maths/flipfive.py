875913261ebdccf7e4320c6e87aff78ad7a926a1
z1chh <lolchen2012@gmail.com>
2022-11-23 00:09:59 -0500

    removed winning

--- src/Kattis/Website/Maths/flipfive.py
+++ src/Kattis/Website/Maths/flipfive.py
@@ -29,13 +29,6 @@ def flipBoard(board, n):
     return ''.join(newBoard)
 
 
-def isWinningBoard(board):
-    for c in board:
-        if c == "*":
-            return False
-    return True
-
-
 def boardAsString(board):
     print(
         f"-------\n|{board[0]} {board[1]} {board[2]}|\n|{board[3]} {board[4]} {board[5]}|\n|{board[6]} {board[7]} {board[8]}|\n-------")
