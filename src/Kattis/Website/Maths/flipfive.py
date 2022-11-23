242cc93ea2deca8f599a2dc38b27caa0f797dc60
z1chh <lolchen2012@gmail.com>
2022-11-23 00:07:10 -0500

    Works for single move games only

--- src/Kattis/Website/Maths/flipfive.py
+++ src/Kattis/Website/Maths/flipfive.py
@@ -8,8 +8,15 @@ Grid:
 | 6 | 7 | 8 |
 +---+---+---+
 """
-neighbors = {0: {0, 1, 3}, 1: {0, 1, 2, 4}, 2: {1, 2, 5}, 3: {0, 3, 4, 6}, 4: {
-    1, 3, 4, 5, 7}, 5: {2, 4, 5, 8}, 6: {3, 6, 7}, 7: {4, 6, 7, 8}, 8: {5, 7, 8}}
+neighbors = {0: {0, 1, 3},
+             1: {0, 1, 2, 4},
+             2: {1, 2, 5},
+             3: {0, 3, 4, 6},
+             4: {1, 3, 4, 5, 7},
+             5: {2, 4, 5, 8},
+             6: {3, 6, 7},
+             7: {4, 6, 7, 8},
+             8: {5, 7, 8}}
 
 
 def flipBoard(board, n):
@@ -29,6 +36,11 @@ def isWinningBoard(board):
     return True
 
 
+def boardAsString(board):
+    print(
+        f"-------\n|{board[0]} {board[1]} {board[2]}|\n|{board[3]} {board[4]} {board[5]}|\n|{board[6]} {board[7]} {board[8]}|\n-------")
+
+
 # For each test case
 for _ in range(int(input())):
     # Get input
@@ -36,20 +48,33 @@ for _ in range(int(input())):
     for _ in range(3):
         goalBoard += input()
 
-    # Storing played boards
+    # Storing played boards and their score
     played = set()
     played.add(".........")
-    queue = dict()
-    queue["........."] = 0
+    storage = dict()
+    storage["........."] = 0
+
+    # Initialize queue with whiteboard
+    queue = ["........."]
 
     # Exhaustive search
-    winScores = []
+    winScores = set()
     while queue:
-        poppedBoard, curScore = queue.pop()
+        poppedBoard = queue.pop(0)
+        # print("popping:")
+        # boardAsString(poppedBoard)
+        curScore = storage[poppedBoard]
         for i in range(9):
             flippedBoard = flipBoard(poppedBoard, i)
             if flippedBoard == goalBoard:
-                winScores.append(curScore)
+                winScores.add(curScore + 1)
             elif flippedBoard not in played:
-                queue.append((flippedBoard, curScore + 1))
-    print(max(winScores))
+                # print(f"adding to queue ({i}):")
+                # boardAsString(flippedBoard)
+                queue.append(flippedBoard)
+                played.add(flippedBoard)
+                if flippedBoard in storage:
+                    storage[flippedBoard] += 1
+                else:
+                    storage[flippedBoard] = 1
+    print(min(winScores))
