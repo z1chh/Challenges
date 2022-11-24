6e14ce7dbbf8c95378726ea8217a7ab826ea6410
z1chh <lolchen2012@gmail.com>
2022-11-23 20:24:33 -0500

    lost in the bigger sauce

--- src/Kattis/Website/Maths/gold.py
+++ src/Kattis/Website/Maths/gold.py
@@ -1,3 +1,9 @@
+# Global variables
+width, height = map(int, input().split())
+validTiles = {'G', '.'}
+
+
+# Helper functions
 def display(grid):
     for line in grid:
         print(' '.join(line))
@@ -15,8 +21,67 @@ def toMatrix(grid, width, height):
     return m
 
 
-width, height = map(int, input().split())
-grid = [[c for c in input()] for _ in range(height)]
+def play(grid, queue, visited):
+    # Get current position
+    h, w = -1, -1
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 'P':
+                h, w = i, j
+                break
+
+    # Make sure that position is found
+    if h == -1 and w == -1:
+        raise ValueError("Error: player not found")
+
+    # Check top
+    if h > 0 and grid[h - 1][w] in validTiles:
+        newGrid = [line.copy() for line in grid]
+        newGrid[h - 1][w] = 'P'
+        newGrid[h][w] = '.'
+        newGrid = toString(newGrid)
+        queue.append(newGrid)
+        visited.add(newGrid)
+
+    # Check bottom
+    if h < width and grid[h + 1][w] in validTiles:
+        newGrid = [line.copy() for line in grid]
+        newGrid[h + 1][w] = 'P'
+        newGrid[h][w] = '.'
+        newGrid = toString(newGrid)
+        queue.append(newGrid)
+        visited.add(newGrid)
+
+    # Check left
+    if w > 0 and grid[h][w - 1] in validTiles:
+        newGrid = [line.copy() for line in grid]
+        newGrid[h][w - 1] = 'P'
+        newGrid[h][w] = '.'
+        newGrid = toString(newGrid)
+        queue.append(newGrid)
+        visited.add(newGrid)
+
+    # Check right
+    if w < width and grid[h][w + 1] in validTiles:
+        newGrid = [line.copy() for line in grid]
+        newGrid[h][w + 1] = 'P'
+        newGrid[h][w] = '.'
+        newGrid = toString(newGrid)
+        queue.append(newGrid)
+        visited.add(newGrid)
 
 
-display(grid)
+# Main function
+def main():
+    grid = [[c for c in input()] for _ in range(height)]
+    queue = [toString(grid)]
+    visited = set()
+
+    while queue:
+        newGrid = queue.pop()
+        visited.add(newGrid)
+
+
+# Run main
+if __name__ == "__main__":
+    main()
