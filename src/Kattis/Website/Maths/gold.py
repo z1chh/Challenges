6ee2593792556c2099ab6344001cf9be0d5539e0
z1chh <lolchen2012@gmail.com>
2022-11-23 21:39:46 -0500

    Organized code

--- src/Kattis/Website/Maths/gold.py
+++ src/Kattis/Website/Maths/gold.py
@@ -1,62 +1,72 @@
-# Get width and height
-width, height = map(int, input().split())
+def gettingGold(grid, width, height, visited):
+    # Set amount of gold to 0
+    gold = 0
 
-# Initialize game map
-grid = [[c for c in input()] for _ in range(height)]
+    # Find start position
+    x, y = -1, -1
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == "P":
+                x, y = i, j
+                break
 
-# Initialize visited tiles
-visited = [[False for _ in range(width)] for _ in range(height)]
+    # Assert position found (should always be found)
+    if x == -1 and y == -1:
+        raise ValueError("Error: player not found")
 
-# Set amount of gold to 0
-gold = 0
+    # Initialize queue and visited tiles
+    queue = [(x, y)]
+    visited[x][y] = True
 
-# Find start position
-x, y = -1, -1
-for i in range(height):
-    for j in range(width):
-        if grid[i][j] == "P":
-            x, y = i, j
-            break
+    # Loop while there are still tiles to visit
+    while queue:
+        x, y = queue.pop(0)
 
-# Assert position found (should always be found)
-if x == -1 and y == -1:
-    raise ValueError("Error: player not found")
+        # Check if gold found
+        if grid[x][y] == "G":
+            gold += 1
 
-# Initialize queue and visited tiles
-queue = [(x, y)]
-visited[x][y] = True
+        # Check if trap nearby
+        if grid[x - 1][y] == "T" or grid[x + 1][y] == "T" or grid[x][y - 1] == "T" or grid[x][y + 1] == "T":
+            continue
 
-# Loop while there are still tiles to visit
-while queue:
-    x, y = queue.pop(0)
+        # Check top
+        if (grid[x - 1][y] == "." or grid[x - 1][y] == "G") and not visited[x - 1][y]:
+            queue.append((x - 1, y))
+            visited[x - 1][y] = True
 
-    # Check if gold found
-    if grid[x][y] == "G":
-        gold += 1
+        # Check bottom
+        if (grid[x + 1][y] == "." or grid[x + 1][y] == "G") and not visited[x + 1][y]:
+            queue.append((x + 1, y))
+            visited[x + 1][y] = True
 
-    # Check if trap nearby
-    if grid[x - 1][y] == "T" or grid[x + 1][y] == "T" or grid[x][y - 1] == "T" or grid[x][y + 1] == "T":
-        continue
+        # Check left
+        if (grid[x][y - 1] == "." or grid[x][y - 1] == "G") and not visited[x][y - 1]:
+            queue.append((x, y - 1))
+            visited[x][y - 1] = True
 
-    # Check top
-    if (grid[x - 1][y] == "." or grid[x - 1][y] == "G") and not visited[x - 1][y]:
-        queue.append((x - 1, y))
-        visited[x - 1][y] = True
+        # Check right
+        if (grid[x][y + 1] == "." or grid[x][y + 1] == "G") and not visited[x][y + 1]:
+            queue.append((x, y + 1))
+            visited[x][y + 1] = True
 
-    # Check bottom
-    if (grid[x + 1][y] == "." or grid[x + 1][y] == "G") and not visited[x + 1][y]:
-        queue.append((x + 1, y))
-        visited[x + 1][y] = True
+    # Return amount of gold found
+    return gold
 
-    # Check left
-    if (grid[x][y - 1] == "." or grid[x][y - 1] == "G") and not visited[x][y - 1]:
-        queue.append((x, y - 1))
-        visited[x][y - 1] = True
 
-    # Check right
-    if (grid[x][y + 1] == "." or grid[x][y + 1] == "G") and not visited[x][y + 1]:
-        queue.append((x, y + 1))
-        visited[x][y + 1] = True
+def main():
+    # Get width and height
+    width, height = map(int, input().split())
 
-# Output amount of gold found
-print(gold)
+    # Initialize game map
+    grid = [[c for c in input()] for _ in range(height)]
+
+    # Initialize visited tiles
+    visited = [[False for _ in range(width)] for _ in range(height)]
+
+    # Solve
+    print(gettingGold(grid, width, height, visited))
+
+
+if __name__ == "__main__":
+    main()
