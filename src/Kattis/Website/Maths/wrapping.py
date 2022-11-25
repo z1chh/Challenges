bc8a74e59f5879daf14ddea181cc0ccfb3747be2
z1chh <lolchen2012@gmail.com>
2022-11-24 21:55:20 -0500

    WA

---
+++ src/Kattis/Website/Maths/wrapping.py
@@ -0,0 +1,8 @@
+sawmill_area = 99.5
+for _ in range(int(input())):
+    boards = 0.0
+    for _ in range(int(input())):
+        _, _, x, y, _ = map(float, input().split())
+        boards += x * y
+    ans = int(boards / sawmill_area * 1000)
+    print(ans / 10, "%")
