844d35211cfe2c89cac3bc57ff5b3b7547b6e068
z1chh <lolchen2012@gmail.com>
2022-11-29 00:03:32 -0500

    bye more random not challenge stuff

--- src/Numbers Theory/GCD.py
+++
@@ -1,8 +0,0 @@
-def GCD(a, N):
-    if a < N:
-        return GCD(N, a)
-    if a % N == 0:
-        return N
-    else:
-        return GCD(N, a % N)
-    
--- src/Numbers Theory/inverse.py
+++
@@ -1,9 +0,0 @@
-from eGCD import *
-
-
-def inverse(a, N):
-    d, X, Y = eGCD(a, N)
-    if d == 1:
-        return X % N
-    else:
-        raise Exception("Error: no inverse")
