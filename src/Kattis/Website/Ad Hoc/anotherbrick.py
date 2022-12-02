df9a8259d8951498eaee09056baf25b119dff5a2
z1chh <lolchen2012@gmail.com>
2022-12-01 19:53:39 -0500

    Another Brick in the Wall: WA

---
+++ src/Kattis/Website/Ad Hoc/anotherbrick.py
@@ -0,0 +1,41 @@
+def anotherbrick(h, w, bricks):
+    # Initialize current row and height
+    curW = 0
+    curH = 0
+
+    # Check if each brick fits
+    for brick in bricks:
+        # Check if annoyed
+        if curW > w:
+            return "NO"
+
+        # Check if go next row
+        if curW == w:
+            curW = 0
+            curH += 1
+
+        # Check if wall completed
+        if curH > h:
+            return "YES"
+
+        # Place brick
+        curW += brick
+
+    # Check if wall completed
+    if curW == w and curH == h + 1:
+        return "YES"
+    else:
+        return "NO"
+
+
+def main():
+    # Get input
+    h, w, _ = map(int, input().split())
+    bricks = map(int, input().split())
+
+    # Compute and output
+    print(anotherbrick(h, w, bricks))
+
+
+if __name__ == "__main__":
+    main()
