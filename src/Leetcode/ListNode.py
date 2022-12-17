a7234384cd5dc1de592dc732e35ef2ba48f5a004
z1chh <lolchen2012@gmail.com>
2022-12-16 23:12:20 -0500

    Implemented reverse ListNode

--- src/Leetcode/ListNode.py
+++ src/Leetcode/ListNode.py
@@ -22,12 +22,40 @@ class ListNode:
             repr += f" -> ({cur.val})"
         return repr
 
+    def reverse(self):
+        # Check if single element
+        if self.next is None:
+            return self
+
+        # Store elements in map
+        idx = 1
+        elements = {}
+        elements[0] = self.val
+        cur = self.next
+        while cur.next is not None:
+            cur = cur.next
+            elements[idx] = cur.val
+            idx += 1
+
+        # Create reverse ListNode
+        idx -= 1
+        reversedList = ListNode(elements[idx])
+
+        # Add elements in reverse order
+        while idx >= 0:
+            reversedList.append(elements[idx])
+            idx -= 1
+
+        # Return reversed ListNode
+        return reversedList
+
 
 def main():
     l = ListNode(0)
     for i in range(1, 5):
         l.add(i)
     print(l)
+    print(l.reverse())
 
 
 if __name__ == "__main__":
