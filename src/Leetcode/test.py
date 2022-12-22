c6b2554d34d57b65d3f32cae0a6508e4f8154b3b
z1chh <lolchen2012@gmail.com>
2022-12-22 00:00:15 -0500

    testing stuff

--- src/Leetcode/test.py
+++ src/Leetcode/test.py
@@ -1,5 +1,45 @@
-a = 5
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
 
-print(f"hi {a}")
-for idx in range(a):
-    print(f"FOR INDEX {idx}")
+    # Initalize k
+    size = len(nums)
+    k = 0
+
+    # Base cases
+    if size < 2:
+        return size
+
+    # Get first element
+    last = nums[0]
+
+    # Remove duplicates
+    dupes = []
+    for i, v in enumerate(nums):
+        if i == 0:
+            continue
+        if v == last:
+            dupes.append(i - k)
+            k += 1
+
+        last = v
+
+    # Test
+    print("dupes", dupes)
+    for i in dupes:
+        nums.pop(i)
+        nums.append(0)
+
+    # Return number of duplicates
+    print(k, nums)
+    return size - k
+
+
+def main():
+    removeDuplicates([1, 1, 2])
+
+
+if __name__ == '__main__':
+    main()
