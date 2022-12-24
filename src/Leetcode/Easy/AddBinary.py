b04772e0cfa3c2675ae820fd824e627ac99f8af4
z1chh <lolchen2012@gmail.com>
2022-12-24 15:24:29 -0500

    Finished Leetcode Problem Add Binary

---
+++ src/Leetcode/Easy/AddBinary.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+
+        # Reverse numbers
+        n1 = a[::-1]
+        n2 = b[::-1]
+
+        # Initialize sum
+        n3 = ""
+
+        # Compute sum
+        carry = False
+        for d1, d2 in zip(n1, n2):
+            if d1 == 0 and d2 == 0:
+                if carry:
+                    n3 += "1"
+                    carry = False
+                else:
+                    n3 += "0"
+            if d1 == 0 and d2 == 1:
+                if carry:
+                    n3 += "0"
+                else:
+                    n3 += "1"
+            if d1 == 1 and d2 == 0:
+                if carry:
+                    n3 += "0"
+                else:
+                    n3 += "1"
+            if d1 == 1 and d2 == 1:
+                if carry:
+                    n3 += "1"
+                else:
+                    n3 += "0"
+                    carry = True
+
+        # Check for carry
+        if carry:
+            n3 += "1"
+
+        # Reverse number
+        n3 = n3[::-1]
+
+        # Return sum
+        return n3
