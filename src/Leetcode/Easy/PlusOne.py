a1f22a5b6c4655f0cd0032d01532b905a5fa08c8
z1chh <lolchen2012@gmail.com>
2022-12-24 15:09:47 -0500

    Leetcode Problem Plus One

---
+++ src/Leetcode/Easy/PlusOne.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+
+        # Initialize vars
+        carry = False
+        temp = []
+
+        # Reverse list
+        digits.reverse()
+
+        # Check if overflow
+        if digits[0] == 9:
+            carry = True
+
+        # Increment number
+        for i, d in enumerate(digits):
+            if d != 9:
+                if carry:
+                    digits[i] += 1
+                    carry = False
+            else:
+                if carry:
+                    digits[i] = 0
+
+        # Check if overflow
+        if carry:
+            digits.pop()
+            digits.append(0)
+            digits.append(1)
+
+        # Reverse list back to normal
+        digits.reverse()
+
+        # Return list
+        return digits
