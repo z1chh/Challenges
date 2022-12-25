5f1703ecbbbc45af21ed558fd6665f79fa86ea33
z1chh <lolchen2012@gmail.com>
2022-12-25 15:44:34 -0500

    Finished Leetcode Problem Divide Two Integers

---
+++ src/Leetcode/Medium/DivideTwoIntegers.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+
+        # Initialize vars
+        remainder = divisor
+        quotient = 0
+        neg = False
+
+        # Remove negative numbers
+        if remainder < 0:
+            remainder = -remainder
+            neg = True
+
+        if divisor < 0:
+            divisor = -divisor
+            neg = not neg
+
+        # Perform division
+        while remainder >= divisor:
+            remainder -= divisor
+            quotient += 1
+
+        # Return quotient
+        return -quotient if neg else quotient
