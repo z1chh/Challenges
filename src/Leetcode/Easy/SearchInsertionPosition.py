adb204843ebaf82ede381d0bc2f3c72bfe345cd4
z1chh <lolchen2012@gmail.com>
2022-12-25 16:08:50 -0500

    Finished Leetcode Problem Search Insert Position

---
+++ src/Leetcode/Easy/SearchInsertionPosition.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        size = len(nums)
+
+        # Edge cases
+        if size == 0:
+            return 0
+        if size == 1:
+            return 0 if nums[0] >= target else 1
+
+        # Initialize vars
+        for i in range(size - 1):
+            first = nums[i]
+            second = nums[i + 1]
+
+            # Check if target found
+            if target == first:
+                return i
+            elif target == second:
+                return i + 1
+
+            # Check if target between first and second
+            if first < target < second:
+                return i + 1
+
+        # Add at the end
+        return size
