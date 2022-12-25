84121160655fcf3cd17482f5ad8a8c677ee33eb5
z1chh <lolchen2012@gmail.com>
2022-12-25 00:34:33 -0500

    Tests

--- src/Leetcode/Misc/moveback.py
+++ src/Leetcode/Misc/moveback.py
@@ -4,9 +4,19 @@ def moveBack(l, i):
         l[i], l[i + 1] = l[i + 1], l[i]
 
 
+# Helper function to move ith element of the list to the back (minus t elements)
+def moveBack2(l, i, t):
+    for i in range(i, len(l) - t - 1):
+        l[i], l[i + 1] = l[i + 1], l[i]
+
+
 def main():
     l = [1, 2, 2, 3, 4]
+    print(l)
     moveBack(l, 1)
+    print(l)
+    l = [1, 2, 2, 3, 4]
+    moveBack2(l, 1, 0)
     print(l)
 
 
