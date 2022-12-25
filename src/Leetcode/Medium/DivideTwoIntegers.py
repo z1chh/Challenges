199a8eb69dfd3afb0243727311659037961215e9
z1chh <lolchen2012@gmail.com>
2022-12-25 15:53:08 -0500

    Added base case handling

--- src/Leetcode/Medium/DivideTwoIntegers.py
+++ src/Leetcode/Medium/DivideTwoIntegers.py
@@ -6,6 +6,12 @@ class Solution(object):
         :rtype: int
         """
 
+        # Edge cases
+        if divisor == 1:
+            return dividend
+        elif divisor == -1:
+            return -dividend
+        
         # Initialize vars
         remainder = dividend
         quotient = 0
