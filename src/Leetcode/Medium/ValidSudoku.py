1778282d3550a932d0ca773343de1a263957593c
z1chh <lolchen2012@gmail.com>
2022-12-25 16:16:40 -0500

    Started Leetcode Problem Valid Sudoku

---
+++ src/Leetcode/Medium/ValidSudoku.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+
+        # Check rows
+        for row in board:
+            digits = set()
+            for d in row:
+                if d != ".":
+                    # Invalid if digit already exists
+                    if d in digits:
+                        return False
+
+                    # Otherwise, add it to the list
+                    else:
+                        digits.add(d)
+
+        # Check columns
+        for col in range(9):
+            digits = set()
+            for row in range(9):
+                # Get digit
+                d = board[row][col]
+                if d != ".":
+                    # Invalid if digit already exists
+                    if d in digits:
+                        return False
+
+                    # Otherwise, add it to the list
+                    else:
+                        digits.add(d)
+
+        # Check sub-boards
